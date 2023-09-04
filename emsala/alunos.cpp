5b5445599ce989d7bbc8f7c0194819c08794ddfa
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2023-09-04 17:52:33 -0300

    vector

--- emsala/alunos.cpp
+++ emsala/alunos.cpp
@@ -1,28 +1,24 @@
 #include <iostream>
+#include <cstring>
+
 using namespace std;
 
 int main(){
 
-    char nomes[3];
+    string nomes[5];
     float notas[6];
     float media = 0;
 
-    for(int i = 0; i < 3; i++){
+    for(int i = 0; i < 5; i++){
     
         cout << "Digite o " << i + 1 << "º nome: " << endl;
-        cin.getline(nomes,3);
-
-        cout << "Digite a " << i + 1 << "º nota: " << endl;
-        cin >> notas[i];
-        cin.get();
-        media = notas[i] / 3;
+        getline(cin, nomes[i]);
 
     }
 
-    for(int i = 0; i < 3; i++){
+    for(int i = 0; i < 5; i++){
         cout << "Nomes = " << nomes[i] << endl;
 
-        cout << "Notas = " << i + 1 << endl;
-
+        // cout << "Notas = " << i + 1 << endl;
     }
 }
