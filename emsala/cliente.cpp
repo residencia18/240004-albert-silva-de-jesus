49721f58b22c49a6c51b23d2b7b5a848378ef889
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2023-09-16 00:14:44 -0300

    Data Atualizada

--- emsala/cliente.cpp
+++ emsala/cliente.cpp
@@ -11,36 +11,60 @@ struct MinhaData
 {
     int dia, mes, ano;
 
+    tm *getTempo()
+    {
+        time_t t;
+        time(&t);
+        struct tm *data;
+        data = localtime(&t);
+        return data;
+    }
+
+    void mostraDataAtual()
+    {
+        struct tm *local = getTempo();
+        int dia = local->tm_mday;
+        int mes = local->tm_mon + 1;
+        int ano = local->tm_year + 1900;
+        int diaSemana = local->tm_wday;
+        int diaAno = local->tm_yday;
+        int hora = local->tm_hour;
+        int minutos = local->tm_min;
+        int segundos = local->tm_sec;
+
+        cout << "\n\tDATA: " << dia << "/" << mes << "/" << ano << " " << diaDaSemana() << endl;
+        cout << "\tHORÁRIO: "<< hora << ":" << minutos << ":" << segundos << endl;
+        cout << endl;
+        // cout << "Dia do ano: " << diaAno << endl;
+    }
+
     void lerData()
     {
         do
         {
-            cout << "\nInforme o dia: ";
+            cout << "\n\tInforme o dia: ";
             cin >> dia;
 
-            cout << "\nInforme o mes: ";
+            cout << "\n\tInforme o mes: ";
             cin >> mes;
 
-            cout << "\nInforme o ano: ";
+            cout << "\n\tInforme o ano: ";
             cin >> ano;
         } while (!validaData());
     }
 
     void mostraData()
     {
-        printf("%02d/%02d/%4d  ", dia, mes, ano);
-        cout << diaDaSemana() << endl;
+        printf("%02d/%02d/%4d\n", dia, mes, ano);
+        // cout << diaDaSemana() << " Horário: " << hora << ":" << minutos << ":" << segundos <<  endl;
     }
 
     int anosCompletos()
     {
-        time_t now;
-        time(&now);
-        struct tm *local = localtime(&now);
+        struct tm *local = getTempo();
         int diaAtual = local->tm_mday;
         int mesAtual = local->tm_mon + 1;
-        int anoAtual = local->tm_year + 1900;
-        int x = anoAtual - ano;
+        int x = (local->tm_year + 1900) - ano;
 
         if (mesAtual < mes)
         {
@@ -56,95 +80,99 @@ struct MinhaData
         return x;
     }
 
-    bool validaData()
+    bool bissexto()
     {
-
-        int meses[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-
-        if (ano % 4 == 0 || (ano % 100 == 0 && ano % 400 == 0))
+        if (ano % 4 == 0 && ano % 100 != 0)
         {
-            meses[1] = 29;
-        }
-
-        {
-            if (dia > 31 || dia < 1 || mes > 12 || mes < 1 || ano < 1)
-            {
-                limparTela();
-                cout << "\nData invalida!" << endl;
-                return false;
-            }
-
-            if (dia > meses[mes - 1])
-            {
-                limparTela();
-                cout << "\nData invalida!" << endl;
-                return false;
-            }
-
             return true;
         }
+        else
+        {
+            if (ano % 400 == 0)
+            {
+                return true;
+            }
+        }
+        return false;
     }
-    
+
+    bool validaData()
+    {
+        limparTela();
+
+        if (ano < 0)
+        {
+            cout << "\n\tOps, Ano invalido!...\n";
+            return false;
+        }
+
+        if (mes < 1 || mes > 12)
+        {
+            cout << "\n\tOps, Mes invalido!...\n";
+            return false;
+        }
+
+        if (dia < 1 || dia > 31)
+        {
+            cout << "\n\tOps, Dia invalido!...\n";
+            return false;
+        }
+
+        if (mes == 2)
+        {
+            if (bissexto())
+            {
+                if (dia > 29)
+                {
+                    cout << "\n\tOps, Dia invalido!...\n";
+                    return false;
+                }
+            }
+            else
+            {
+                if (dia > 28)
+                {
+                    cout << "\n\tOps, Dia invalido!...\n";
+                    return false;
+                }
+            }
+        }
+
+        if (mes == 4 || mes == 6 || mes == 9 || mes == 11)
+        {
+            if (dia > 30)
+            {
+                cout << "\n\tOps, Dia invalido!...\n";
+                return false;
+            }
+        }
+
+        return true;
+    }
+
     string diaDaSemana()
     {
+        struct tm *data = getTempo();; 
+        int diaDaSemana = data->tm_wday;
 
-        int ds;
-        int g;
-        int f;
-        int delta;
-        int n;
-        float frac;
-        float intpart;
-
-        if (mes > 2)
+        switch (diaDaSemana)
         {
-            g = ano;
-            f = mes + 1;
-        }
-        else if (mes <= 2)
-        {
-            g = ano - 1;
-            f = mes + 13;
-        }
-
-        n = int(365.25 * g) + int(30.6 * f) - 621049 + dia;
-
-        if (n < 36523)
-        {
-            delta = 2;
-        }
-        else if (36523 <= n and n < 73048)
-        {
-            delta = 1;
-        }
-        else if (n >= 73048)
-        {
-            delta = 0;
-        }
-
-        frac = modf((float)n / 7, &intpart);
-
-        ds = round(frac * 7) + delta + 1;
-
-        switch (ds)
-        {
+        case 0:
+            return "DOMINGO";
         case 1:
-            return "domingo";
+            return "SEGUNDA-FEIRA";
         case 2:
-            return "segunda-feira";
+            return "TERÇA-FEIRA";
         case 3:
-            return "terca-feira";
+            return "QUARTA-FEIRA";
         case 4:
-            return "quarta-feira";
+            return "QUINTA-FEIRA";
         case 5:
-            return "quinta-feira";
+            return "SEXTA-FEIRA";
         case 6:
-            return "sexta-feira";
-        case 7:
-            return "sabado";
+            return "SABADO";
         }
-
-        return "dia invalido";
+        return "ERRO";
     }
 };
 
@@ -158,17 +186,19 @@ typedef struct
     void cadastraVeiculo(int i)
     {
         limparTela();
-        cout << "\n==========CADASTRO DE VEICULO==========\n"
+        dataRegistro.mostraDataAtual();
+
+        cout << "\t==========CADASTRO DE VEICULO==========\n"
              << endl;
 
-        cout << i << "º Veiculo";
-        cout << "\nInforme o modelo do veículo: ";
+        cout << "\t" << i << "º Veiculo";
+        cout << "\n\tInforme o modelo do veículo: ";
         cin >> modelo;
 
-        cout << "\nInforme a cor do veículo: ";
+        cout << "\n\tInforme a cor do veículo: ";
         cin >> cor;
 
-        cout << "\nInforme a placa do veiculo: ";
+        cout << "\n\tInforme a placa do veiculo: ";
         cin >> placa;
 
         dataRegistro.lerData();
@@ -186,19 +216,19 @@ typedef struct
 
     void lerNome()
     {
-        cout << "\nInforme o nome do cliente: ";
+        cout << "\n\tInforme o nome do cliente: ";
         getline(cin, nome);
     }
 
     void lerSobrenome()
     {
-        cout << "\nInforme o sobrenome: ";
+        cout << "\n\tInforme o sobrenome: ";
         getline(cin, sobrenome);
     }
 
     void lerCpf()
     {
-        cout << "\nInforme o cpf: ";
+        cout << "\n\tInforme o cpf: ";
         cin >> cpf;
     }
 
@@ -206,22 +236,22 @@ typedef struct
     {
         int i = 1;
 
-        cout << "\n"
+        cout << "\n\t"
              << j << "º CLIENTE";
-        cout << "\nNome do Cliente: " << nome << endl;
-        cout << "Sobrenome: " << sobrenome << endl;
-        cout << "CPF do Cliente: " << cpf << endl;
-        cout << "Data de Nascimento: ";
+        cout << "\n\tNome do Cliente: " << nome << endl;
+        cout << "\tSobrenome: " << sobrenome << endl;
+        cout << "\tCPF do Cliente: " << cpf << endl;
+        cout << "\tData de Nascimento: ";
         dataNascimento.mostraData();
 
         for (auto it = veiculos.begin(); it != veiculos.end(); it++, i++)
         {
-            cout << "\n"
+            cout << "\n\t"
                  << i << "º VEICULO";
-            cout << "\nModelo do Carro: " << it->modelo << endl;
-            cout << "Cor do veiculo: " << it->cor << endl;
-            cout << "Placa do veiculo: " << it->placa << endl;
-            cout << "Data de Registro: ";
+            cout << "\n\tModelo do Carro: " << it->modelo << endl;
+            cout << "\tCor do veiculo: " << it->cor << endl;
+            cout << "\tPlaca do veiculo: " << it->placa << endl;
+            cout << "\tData de Registro: ";
             it->dataRegistro.mostraData();
         }
     }
@@ -232,7 +262,8 @@ typedef struct
         Veiculo veiculo;
         int quantidadeDeCarros = 0;
 
-        cout << "\n================CLIENTE=================\n";
+        dataNascimento.mostraDataAtual();
+        cout << "\t================CLIENTE=================\n";
 
         lerNome();
 
@@ -245,9 +276,11 @@ typedef struct
         if (dataNascimento.anosCompletos() >= 18)
         {
             limparTela();
-            cout << "\n==========CADASTRO DE VEICULO==========\n";
+            dataNascimento.mostraDataAtual();
 
-            cout << "\nInforme a quantidade de veiculos: ";
+            cout << "\t==========CADASTRO DE VEICULO==========\n";
+
+            cout << "\n\tInforme a quantidade de veiculos: ";
             cin >> quantidadeDeCarros;
             cin.get();
 
@@ -263,7 +296,9 @@ typedef struct
     void lerDataNascimento()
     {
         limparTela();
-        cout << "\n==========DATA DE NASCIMENTO==========\n";
+        dataNascimento.mostraDataAtual();
+
+        cout << "\t==========DATA DE NASCIMENTO==========\n";
 
         dataNascimento.lerData();
     }
@@ -320,11 +355,11 @@ int main()
 
         case 0:
 
-            cout << "\nPrograma encerrado com sucesso!..." << endl;
+            cout << "\n\tPrograma encerrado com sucesso!..." << endl;
             exit(0);
 
         default:
-            cout << "Opção invalida!";
+            cout << "\tOps, Opção invalida!";
         }
 
     } while (escolha != 0);
@@ -333,25 +368,19 @@ int main()
 int menu()
 {
     limparTela();
+    MinhaData dataAtual;
     int opcao = 0;
 
     do
-    {
-        time_t now;
-        time(&now);
-        struct tm *local = localtime(&now);
-        int dia = local->tm_mday;
-        int mes = local->tm_mon + 1;
-        int ano = local->tm_year + 1900;
-
-        printf("Hoje é dia %2d/%02d/%2d", dia, mes, ano);
-        cout << "\n===============MENU===============";
-        cout << "\n[1] - NOVO CLIENTE:";
-        cout << "\n[2] - ENCONTRAR CLIENTE:";
-        cout << "\n[3] - EXCLUIR CLIENTE:";
-        cout << "\n[4] - LISTAR TODOS OS CLIENTES:";
-        cout << "\n[0] - SAIR";
-        cout << "\nENTRADA ->  ";
+    {   
+        dataAtual.mostraDataAtual();
+        cout << "\t===============MENU===============";
+        cout << "\n\t[1] - NOVO CLIENTE:";
+        cout << "\n\t[2] - ENCONTRAR CLIENTE:";
+        cout << "\n\t[3] - EXCLUIR CLIENTE:";
+        cout << "\n\t[4] - LISTAR TODOS OS CLIENTES:";
+        cout << "\n\t[0] - SAIR";
+        cout << "\n\tENTRADA ->  ";
         cin >> opcao;
         cin.get();
 
@@ -393,13 +422,16 @@ void cadastraCliente(vector<Cliente> &listCliente)
 void listarClientes(vector<Cliente> &listCliente)
 {
     limparTela();
+    MinhaData dataAtual;
+    dataAtual.mostraDataAtual();
+
     int i = 1;
-    cout << "\n==========LISTA DE CLIENTES===========\n";
+    cout << "\n\t==========LISTA DE CLIENTES===========\n";
 
     for (auto it = listCliente.begin(); it != listCliente.end(); it++, i++)
     {
         it->mostraDadosCliente(i);
-        cout << "======================================\n";
+        cout << "\t======================================\n";
     }
     pause();
 }
@@ -410,7 +442,7 @@ void buscarCliente(vector<Cliente> &listCliente)
     string cpf;
     int i = 1;
 
-    cout << "Informe o CPF para consulta: ";
+    cout << "\tInforme o CPF para consulta: ";
     getline(cin, cpf);
 
     for (auto it = listCliente.begin(); it != listCliente.end(); ++it, i++)
@@ -424,18 +456,20 @@ void buscarCliente(vector<Cliente> &listCliente)
     }
 
     limparTela();
-    cout << "\nCliente não encontrado!...\n";
+    cout << "\n\tOps, Cliente não encontrado!...\n";
     pause();
 }
 
 void removerCliente(vector<Cliente> &listCliente)
 {
     limparTela();
+    MinhaData dataAtual;
+    dataAtual.mostraDataAtual();
     string cpf;
     int i = 1;
 
-    cout << "\n===============REMOVER CLIENTE============\n";
-    cout << "\nInforme o CPF para remoção: ";
+    cout << "\n\t===============REMOVER CLIENTE============\n";
+    cout << "\n\tInforme o CPF para remoção: ";
     getline(cin, cpf);
 
     for (auto it = listCliente.begin(); it != listCliente.end(); ++it, i++)
@@ -443,13 +477,13 @@ void removerCliente(vector<Cliente> &listCliente)
         if (it->cpf == cpf)
         {
             listCliente.erase(it);
-            cout << "\nCliente removido com sucesso!...\n";
+            cout << "\n\tCliente removido com sucesso!...\n";
             pause();
             return;
         }
     }
 
     limparTela();
-    cout << "\nCliente não encontrado!...\n";
+    cout << "\n\tOps, Cliente não encontrado!...\n";
     pause();
 }
