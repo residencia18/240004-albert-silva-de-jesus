7cb14ba0208d3a6a3cae127a425f72af344306e3
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2023-10-26 15:11:33 -0300

    ListaSalarios, ListaIdades

--- DataFruta.cpp
+++ DataFruta.cpp
@@ -1,28 +1,33 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
-class Data {
+class Data
+{
 	int dia, mes, ano;
-	public:
-	
+
+public:
 	/*
 	O metodo abaixo retornar -1 se d1 anterior a d2
 	Retornar 0 se d1 = d2
 	Retornar +1 se d1 posterior a d2
-	*/	
-	static int compara(Data d1, Data d2) { 
+	*/
+	static int compara(Data d1, Data d2)
+	{
 		return 0;
 	}
-	
-	Data (int _dia, int _mes, int _ano) {
+
+	Data(int _dia, int _mes, int _ano)
+	{
 		dia = _dia;
 		mes = _mes;
 		ano = _ano;
 	}
-	string toString() {
+	string toString()
+	{
 		string ret = "";
 		ret.append(to_string(dia));
 		ret.append("/");
@@ -33,142 +38,381 @@ class Data {
 	}
 };
 
-class Lista {
-	public:
-	virtual void entradaDeDados() =0;
-	virtual void mostraMediana() =0;
-	virtual void mostraMenor() =0;
-	virtual void mostraMaior() =0;
+class Lista
+{
+public:
+	virtual void listarEmOrdem() = 0;
+	virtual void listarEmQuantidade(int quant) = 0;
+	virtual void entradaDeDados() = 0;
+	virtual void mostraMediana() = 0;
+	virtual void mostraMenor() = 0;
+	virtual void mostraMaior() = 0;
 };
 
-class ListaNomes {
+class ListaNomes : public Lista
+{
 	vector<string> lista;
-	
-	public:
-	
+
+public:
 	/*
 	O metodo abaixo pergunta ao usuarios quantos
 	elementos vo existir na lista e depois
 	solicita a digitar o de cada um deles
-	*/	
-	void entradaDeDados() {
+	*/
+	void entradaDeDados()
+	{
 		lista.push_back("Teste");
 	}
-		
-	void mostraMediana() {
+
+	void mostraMediana()
+	{
 		cout << "Aqui vai mostrar a mediana da lista de strings" << endl;
 	}
-	
-	void mostraMenor() {
+
+	void mostraMenor()
+	{
 		cout << "Aqui vai mostrar o primeiro nome alfabeticamente" << endl;
 	}
-	void mostraMaior() {
+	void mostraMaior()
+	{
 		cout << "aqui vai mostrar o ultimo nome alfabeticamente" << endl;
 	}
 };
 
-class ListaDatas  {
+class ListaDatas : public Lista
+{
 	vector<Data> lista;
-	
-	public:
-		
+
+public:
 	/*
 	O metodo abaixo pergunta ao usuarios quantos
 	elementos, existir na lista e depois
 	solicita a digitar de cada um deles
-	*/	
-	void entradaDeDados() {
-		
+	*/
+	void entradaDeDados()
+	{
 	}
-	
-	void mostraMediana() {
+
+	void mostraMediana()
+	{
 		cout << "Aqui vai mostrar a mediana da lista de datas" << endl;
 	}
-	
-	void mostraMenor() {
+
+	void mostraMenor()
+	{
 		cout << "Aqui vai mostrar a primeira data cronologicamente" << endl;
 	}
-	void mostraMaior() {
+	void mostraMaior()
+	{
 		cout << "aqui vai mostrar a ultima data cronologicamente" << endl;
 	}
 };
 
-class ListaSalarios  {
+class ListaSalarios : public Lista
+{
+private:
 	vector<float> lista;
-	
-	public:
-	
+
+	static bool compara(float a, float b)
+	{
+		return a < b;
+	}
+
+public:
+	vector<float> *getLista()
+	{
+		return &(lista);
+	}
+
 	/*
 	O metodo abaixo pergunta ao usuarios quantos
 	elementos, existir na lista e depois
 	solicita a digitar de cada um deles
-	*/	
-	void entradaDeDados() {
-		
+	*/
+	void entradaDeDados() override
+	{
+		int n = 0;
+		float salario = 0;
+
+		do
+		{
+			cout << "\n\tInforme a quantidade de elementos do vetor: ";
+			cin >> n;
+			cin.get();
+
+			if (n <= 0)
+			{
+				cout << "\n\tOps, quantidade de elementos do vetor incorreto!...";
+				cout << "\n\tPressione qualquer tecla para continuar...";
+				cin.get();
+			}
+
+		} while (n <= 0);
+
+		for (int i = 0; i < n; i++)
+		{
+			do
+			{
+				cout << "\n\tInforme o " << i + 1 << "º salário: ";
+				cin >> salario;
+				cin.get();
+
+				if (salario <= 0)
+				{
+					cout << "\n\tOps, salário invalido!...";
+					cout << "\n\tPressione qualquer tecla para continuar...";
+					cin.get();
+				}
+			} while (salario <= 0);
+
+			getLista()->push_back(salario);
+		}
 	}
-			
-	void mostraMediana() {
-		cout << "Aqui vai mostrar a mediana da lista de salarios" << endl;
+
+	void mostraMediana() override
+	{
+		vector<float> aux;
+		aux = *getLista();
+		sort(aux.begin(), aux.end(), compara);
+		float mediana;
+
+		if (aux.size() % 2 == 0)
+		{
+			float posMedianaEsquerda, posMedianaDireita;
+			float medianaEsquerda, medianaDireita;
+			posMedianaEsquerda = aux.size() / 2;
+			posMedianaDireita = aux.size() / 2 + 1;
+
+			medianaEsquerda = aux.at(posMedianaEsquerda - 1);
+			medianaDireita = aux.at(posMedianaDireita - 1);
+			mediana = (medianaDireita + medianaEsquerda) / 2;
+		}
+		else
+		{
+			float pos = aux.size() / 2;
+			mediana = aux.at(pos);
+		}
+		cout << "\n\tAqui vai mostrar a mediana da lista de salários" << endl;
+		cout << "\n\tMediana: " << mediana << endl;
+		cout << "\n\tPressione qualquer tecla para continuar...";
+		cin.get();
 	}
-	
-	void mostraMenor() {
-		cout << "Aqui vai mostrar o menor dos salarios" << endl;
+
+	void mostraMenor() override
+	{
+		vector<float> auxMenor;
+		auxMenor = *getLista();
+		sort(auxMenor.begin(), auxMenor.end(), compara);
+		float menor;
+
+		menor = auxMenor.front();
+
+		cout << "\n\tAqui vai mostrar o menor dos salarios" << endl;
+		cout << "\n\tMenor: " << menor << endl;
+		cout << "\n\tPressione qualquer tecla para continuar...";
+		cin.get();
 	}
-	void mostraMaior() {
-		cout << "aqui vai mostrar o maior dos salarios" << endl;
+
+	void mostraMaior() override
+	{
+		vector<float> auxMaior;
+		auxMaior = *getLista();
+		sort(auxMaior.begin(), auxMaior.end(), compara);
+		float maior;
+
+		maior = auxMaior.back();
+		cout << "\n\tAqui vai mostrar o maior dos salários" << endl;
+		cout << "\n\tMaior: " << maior << endl;
+		cout << "\n\tPressione qualquer tecla para continuar...";
+		cin.get();
+	}
+
+	void listarEmOrdem() override
+	{
+		vector<float> salarioOrdenado;
+		salarioOrdenado = *getLista();
+		sort(salarioOrdenado.begin(), salarioOrdenado.end(), compara);
+
+		for (auto it = salarioOrdenado.begin(); it != salarioOrdenado.end(); it++)
+		{
+			cout << "\n\t" << *it;
+		}
+	}
+
+	void listarEmQuantidade(int quant) override
+	{
+		int i = 0;
+		for (auto it = getLista()->begin(); i < quant && it != getLista()->end(); it++)
+		{
+			cout << "\n\t" << *it;
+			i++;
+		}
 	}
 };
 
+class ListaIdades : public Lista
+{
 
-class ListaIdades  {
+private:
 	vector<int> lista;
-	
-	public:
-		
-		/*
-	O metodo abaixo pergunta ao usuarios quantos
-	elementos existir na lista e depois
-	solicita a digitar de cada um deles
-	*/	
-	void entradaDeDados() {
-		
+
+	static bool compara(int a, int b)
+	{
+		return a < b;
 	}
-	
-	void mostraMediana() {
-		cout << "Aqui vai mostrar a mediana da lista de idades" << endl;
+
+public:
+	vector<int> *getLista()
+	{
+		return &(lista);
 	}
-	
-	void mostraMenor() {
-		cout << "Aqui vai mostrar a menor das idades" << endl;
+
+	/*
+O metodo abaixo pergunta ao usuarios quantos
+elementos existi na lista e depois
+solicita a digitar de cada um deles
+*/
+	void entradaDeDados() override
+	{
+		int n = 0;
+		int idade = 0;
+
+		do
+		{
+			cout << "\n\tInforme a quantidade de elementos do vetor: ";
+			cin >> n;
+			cin.get();
+
+			if (n <= 0)
+			{
+				cout << "\n\tOps, quantidade de elementos do vetor incorreto!...";
+				cout << "\n\tPressione qualquer tecla para continuar...";
+				cin.get();
+			}
+
+		} while (n <= 0);
+
+		for (int i = 0; i < n; i++)
+		{
+			do
+			{
+				cout << "\n\tInforme a " << i + 1 << "º idade: ";
+				cin >> idade;
+				cin.get();
+
+				if (idade <= 0)
+				{
+					cout << "\n\tOps, idade invalida!...";
+					cout << "\n\tPressione qualquer tecla para continuar...";
+					cin.get();
+				}
+
+			} while (idade <= 0);
+
+			getLista()->push_back(idade);
+		}
 	}
-	void mostraMaior() {
-		cout << "aqui vai mostrar a maior das idades" << endl;
+
+	void mostraMediana() override
+	{
+		vector<int> aux;
+		aux = *getLista();
+		sort(aux.begin(), aux.end(), compara);
+		int mediana;
+
+		if (aux.size() % 2 == 0)
+		{
+			int posMedianaEsquerda, posMedianaDireita;
+			int medianaEsquerda, medianaDireita;
+			posMedianaEsquerda = aux.size() / 2;
+			posMedianaDireita = aux.size() / 2 + 1;
+
+			medianaEsquerda = aux.at(posMedianaEsquerda - 1);
+			medianaDireita = aux.at(posMedianaDireita - 1);
+			mediana = (medianaDireita + medianaEsquerda) / 2;
+		}
+		else
+		{
+			int pos = aux.size() / 2;
+			mediana = aux.at(pos);
+		}
+		cout << "\n\tAqui vai mostrar a mediana da lista de idades" << endl;
+		cout << "\n\tMediana: " << mediana << endl;
+		cout << "\n\tPressione qualquer tecla para continuar...";
+		cin.get();
+	}
+
+	void mostraMenor() override
+	{
+		vector<int> auxMenor;
+		auxMenor = *getLista();
+		sort(auxMenor.begin(), auxMenor.end(), compara);
+		int menor;
+
+		menor = auxMenor.front();
+
+		cout << "\n\tAqui vai mostrar a menor das idades" << endl;
+		cout << "\n\tMenor: " << menor << endl;
+		cout << "\n\tPressione qualquer tecla para continuar...";
+		cin.get();
+	}
+
+	void mostraMaior() override
+	{
+		vector<int> auxMaior;
+		auxMaior = *getLista();
+		sort(auxMaior.begin(), auxMaior.end(), compara);
+		int maior;
+
+		maior = auxMaior.back();
+		cout << "\n\tAqui vai mostrar a maior das idades" << endl;
+		cout << "\n\tMaior: " << maior << endl;
+		cout << "\n\tPressione qualquer tecla para continuar...";
+		cin.get();
+	}
+
+	void listarEmOrdem() override
+	{
+		vector<int> ordenado;
+		ordenado = *getLista();
+		sort(ordenado.begin(), ordenado.end(), compara);
+
+		for (auto it = ordenado.begin(); it != ordenado.end(); it++)
+		{
+			cout << "\n\t" << *it;
+		}
+	}
+
+	void listarEmQuantidade(int quant) override
+	{
+		int i = 0;
+		for (auto it = getLista()->begin(); i < quant && it != getLista()->end(); it++)
+		{
+			cout << "\n\t" << *it;
+			i++;
+		}
 	}
 };
- 
-int main () {
-	vector<Lista*> listaDeListas;
-	
-	ListaNomes listaNomes;
-	listaNomes.entradaDeDados();
-	listaDeListas.push_back(&listaNomes);
-	
-	ListaDatas listaDatas;
-	listaDatas.entradaDeDados();
-	listaDeListas.push_back(&listaDatas);
-	
+
+int main()
+{
+	vector<Lista *> listaDeListas;
+	int quant = 2;
+
 	ListaSalarios listaSalarios;
 	listaSalarios.entradaDeDados();
 	listaDeListas.push_back(&listaSalarios);
-	
+
 	ListaIdades listaIdades;
 	listaIdades.entradaDeDados();
 	listaDeListas.push_back(&listaIdades);
-	
-	for (Lista* l : listaDeListas) {
+
+	for (Lista *l : listaDeListas)
+	{
 		l->mostraMediana();
 		l->mostraMenor();
 		l->mostraMaior();
+		l->listarEmOrdem();
+		l->listarEmQuantidade(quant);
 	}
-	
 }
