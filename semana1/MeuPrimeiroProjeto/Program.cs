2e6ee187428a7623be9d4674939f3611f9d2bb53
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2023-11-09 17:17:47 -0300

    MeuPrimeiroProjeto

---
+++ semana1/MeuPrimeiroProjeto/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+// Estudar exceções
+
+#region Teste de tipo de dados
+
+#region Operadores
+
+#endregion
