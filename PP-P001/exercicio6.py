cf845bd8c1c61b3cc5f04f97af5568394e2e5ba0
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2023-11-09 23:46:12 -0300

    exercicio6 Concluido com sucesso

--- PP-P001/exercicio6.py
+++ PP-P001/exercicio6.py
@@ -6,7 +6,7 @@ Exercício 6: Manipulando listas
 L = [1,2,3,4,5,6,7,8,9]
 print(L[::-1])
 print(L[-1::])
-print([:-1:])
+print(L[:-1:])
 print(L[::-2])
 print(L[-2::])
 print(L[:-2:])
@@ -14,19 +14,52 @@ print(L[:-2:])
 ● Uma forma simplificada de determinar o seu animal no zodiaco chines é apresentada na seguinte tabela:
 
 ano do nascimento % 12 | signo
-        1              | macaco
-        2              | galo
-        3              | cão
-        4              | porco
-        5              | rato
-        6              | boi
-        7              | tigre
-        8              | coelho
-        9              | dragão
-        10             | serpente
-        11             | cavalo
-        12             | carneiro
+        0              | macaco
+        1              | galo
+        2              | cão
+        3              | porco
+        4              | rato
+        5              | boi
+        6              | tigre
+        7              | coelho
+        8              | dragão
+        9              | serpente
+        10             | cavalo
+        11             | carneiro
         
 ● Descubra qual o signo de um usuário de acordo com seu ano de nascimento.
 
 '''
+print('\n\t='+'=' *30)
+print('\tManipulando listas:\n')
+
+L = [1,2,3,4,5,6,7,8,9]
+print('\t{}'.format(L[::-1]))
+print('\t{}'.format(L[-1::]))
+print('\t{}'.format(L[:-1:]))
+print('\t{}'.format(L[::-2]))
+print('\t{}'.format(L[-2::]))
+print('\t{}'.format(L[:-2:]))
+
+print('\n\t='+'=' *50)
+print('\tSigno de um usuário de acordo com seu ano de nascimento:\n')
+
+anoDeNascimento = int(input('\tDigite o seu ano de nascimento: '))
+
+switch_meses = {
+    0: lambda: 'Macaco',
+    1: lambda: 'Galo',
+    2: lambda: 'Cão',
+    3: lambda: 'Porco',
+    4: lambda: 'Rato',
+    5: lambda: 'Boi',
+    6: lambda: 'Tigre',
+    7: lambda: 'Coelho',
+    8: lambda: 'Dragão',
+    9: lambda: 'Serpente',
+    10: lambda: 'Cavalo',
+    11: lambda: 'Carneiro'
+}   
+
+print('\n\tSigno: {}'.format(switch_meses.get(anoDeNascimento % 12, lambda: "Ano de Nascimento Invalido!")()))
+print('\t='+'=' *50, '\n')
