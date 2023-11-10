4530276627cb4b17ca600e1a9281461d88165398
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2023-11-09 21:31:39 -0300

    exercicio3 e exercicio4 finalizado

---
+++ PP-P001/exercicio5.py
@@ -0,0 +1,15 @@
+'''
+Exercício 5: Manipulação de variáveis de ponto flutuante, explorando as características e os limites.
+
+● Durante a aula foi apresentado o tipo de dado que permite representar um subconjunto dos números de ponto flutuante. Sobre estes tipos 
+de dados:
+
+● Demonstre como funcionam os operadores aritméticos e aritméticos compostos em Python;
+
+● Utilizando o operador de exponenciação mostre qual a maior e a menor potência de 2 que pode ser representada com variáveis de ponto 
+flutuante.
+
+● As variáveis numéricas são imutáveis. Demonstre com exemplos as implicações desta afirmação;
+
+● Verifique quais métodos estão disponíveis para as variáveis de ponto flutuante;
+'''
---
+++ PP-P001/exercicio6.py
@@ -0,0 +1,32 @@
+'''
+Exercício 6: Manipulando listas
+
+● Dado o seguinte código, e antes de implementar, veja o que será impresso em cada caso:
+
+L = [1,2,3,4,5,6,7,8,9]
+print(L[::-1])
+print(L[-1::])
+print([:-1:])
+print(L[::-2])
+print(L[-2::])
+print(L[:-2:])
+
+● Uma forma simplificada de determinar o seu animal no zodiaco chines é apresentada na seguinte tabela:
+
+ano do nascimento % 12 | signo
+        1              | macaco
+        2              | galo
+        3              | cão
+        4              | porco
+        5              | rato
+        6              | boi
+        7              | tigre
+        8              | coelho
+        9              | dragão
+        10             | serpente
+        11             | cavalo
+        12             | carneiro
+        
+● Descubra qual o signo de um usuário de acordo com seu ano de nascimento.
+
+'''
