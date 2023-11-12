af2419450087e96c6e09f3c208bfa3d750c112bc
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2023-11-12 11:23:57 -0300

    Tipos de Dados e Conversão de Tipos de Dados, atualizados no projeto.

--- P001/Program.cs
+++ P001/Program.cs
@@ -1,2 +1,76 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using System;
+
+namespace P001
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // NcursesDriver.NcursesInit();
+
+      Console.WriteLine("\n\t================================================================================================");
+      Console.WriteLine("\tTIPOS DE DADOS DO C#\n");
+
+      sbyte valor = 42;
+      Console.WriteLine("\tRepresenta números inteiros de 8 bits. (sbyte) = " + valor);
+
+      byte valor2 = 200;
+      Console.WriteLine("\tRepresenta números inteiros de 8 bits sem sinal. (byte) = " + valor2);
+
+      short valor3 = -15000;
+      Console.WriteLine("\tRepresenta números inteiros de 16 bits. (short) = " + valor3);
+
+      ushort valor4 = 30000;
+      Console.WriteLine("\tRepresenta números inteiros de 16 bits sem sinal. (ushort) = " + valor4);
+
+      int valor5 = 1000000;
+      Console.WriteLine("\tRepresenta números inteiros de 32 bits. (int) = " + valor5);
+
+      uint valor6 = 4000000000;
+      Console.WriteLine("\tRepresenta números inteiros de 32 bits sem sinal. (uint) = " + valor6);
+
+      long valor7 = 123456789012345;
+      Console.WriteLine("\tRepresenta números inteiros de 64 bits. (long) = " + valor7);
+
+      ulong valor8 = 18000000000000000000;
+      Console.WriteLine("\tRepresenta números inteiros de 64 bits sem sinal. (ulong) = " + valor8);
+
+      // Pausa a execução do programa até que uma tecla seja pressionada
+      Console.ReadKey();
+      // Limpa a tela
+      Console.Clear();
+
+      Console.WriteLine("\n\t================================================================================================");
+      Console.WriteLine("\tCONVERSÃO DE TIPOS DE DADOS DO C#");
+
+      double valorDouble = 10.75;
+      int valorInteiro = (int)valorDouble;
+      Console.WriteLine("\n\tdouble valorDouble = 10.75;\n\tint valorInteiro = (int)valorDouble;\n\tEm C#, você pode converter uma variável do tipo double para int utilizando um tipo de cast. (int) = " + valorInteiro);
+      Console.WriteLine("\n\tNeste exemplo, valorDouble é uma variável do tipo double com a parte fracionária representando .75. \n\tAo converter para int usando (int)valorDouble, a parte decimal será truncada, resultando em 10 como valor inteiro");
+      Console.WriteLine("\n\tA função Math.Round é usada para arredondar um número para o inteiro mais próximo ou para um número especificado de casas decimais. \n\tO método Math.Round é estático, portanto, você não precisa criar uma instância de Math para usá-lo. \n\tO método Math.Round tem vários sobrecargas. \n\tAqui, usamos o método Math.Round (double, int) para arredondar o valorDouble para o inteiro mais próximo. \n\tO valor de retorno é do tipo double, portanto, precisamos converter para int usando (int). \n\t(int)Math.Round(valorDouble) = " + (int)Math.Round(valorDouble));
+
+      Console.ReadKey();
+
+      Console.WriteLine("\n\t================================================================================================");
+      Console.WriteLine("\tOPERADORES ARITMÉTICOS DO C#");
+
+      Console.WriteLine("\n\t================================================================================================");
+      Console.WriteLine("\tOPERADORES DE COMPARAÇÃO DO C#");
+
+      Console.WriteLine("\n\t================================================================================================");
+      Console.WriteLine("\tOPERADORES DE IGUALDADE DO C#");
+
+      Console.WriteLine("\n\t================================================================================================");
+      Console.WriteLine("\tOPERADORES LÓGICOS DO C#");
+
+      Console.WriteLine("\n\t================================================================================================");
+      Console.WriteLine("\tDESAFIO DE MISTURA DE OPERADORES DO C#");
+
+      Console.WriteLine("\n\t================================================================================================\n\n");
+
+      // NcursesDriver.NcursesEnd();
+    }
+  }
+}
