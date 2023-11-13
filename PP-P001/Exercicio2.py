751c3235ab9cab790ef8f729f6288781bb425431
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2023-11-12 22:14:54 -0300

    Projeto Atualizado

--- PP-P001/Exercicio2.py
+++ PP-P001/Exercicio2.py
@@ -14,6 +14,22 @@ C/C++;
 ● Verifique quais métodos estão disponíveis para as variáveis inteiras; 
 
 '''
+import os
+import platform
+
+def pause():
+      input("\tPressione Enter para continuar...")
+
+def limpaTela():
+    sistema_operacional = platform.system().lower()
+
+    if sistema_operacional == "windows":
+      os.system("cls")
+    elif sistema_operacional == "linux":
+      os.system("clear")
+    else:
+      print("Sistema operacional não suportado para limpar a tela.")
+        
 print('\n\t='+'=' *60)
 print('\t+ - * // % Operadores Aritméticos em Python + - * // %')
 
@@ -41,7 +57,10 @@ print('\tDivisão inteira: {} // {} = {}'.format(a, b, a // b))
 print('\tResto da divisão: {} % {} = {}'.format(a, b, a % b))
 
 print('\t='+'=' *60)
+pause()
+limpaTela()
 
+print('\t='+'=' *70)
 print('\t+= -= *= /= Operadores Aritméticos Compostos em Python += -= *= /=')
 
 # Adição com atribuição
@@ -66,15 +85,23 @@ print('\n\tx = {}\n\ty = {}\n\tx /= y'.format(x, y))
 x /= y
 print('\tDivisão com atribuição {:.0f}'.format(x))
 
-print('\t='+'=' *100)
+print('\t='+'=' *70)
+pause()
+limpaTela()
+
+print('\t='+'=' *110)
 print('\tPrincipais diferenças em relação a C/C++:\n'
       '\n\tDivisão padrão retorna float: Em Python, a divisão de dois inteiros resulta em um número de ponto flutuante, \n\ta menos que você use o operador de divisão inteira //. Em C/C++, a divisão de inteiros resulta em um número inteiro, \n\te se você quiser um resultado de ponto flutuante, pelo menos um dos operandos deve ser do tipo float.\n'
       '\n\tSem restrições de tamanho em inteiros: Em Python, os inteiros não têm um tamanho fixo, \n\to que significa que você pode trabalhar com inteiros de qualquer tamanho, sem preocupações com estouro. Em C/C++, \n\to tamanho dos inteiros é limitado pela arquitetura do sistema.\n'
       '\n\tOperadores compostos simplificados: Python oferece operadores compostos mais expressivos, como +=, -= etc., \n\ttornando o código mais conciso em comparação com C/C++.\n'
       '\n\tExponenciação mais intuitiva: O operador ** em Python é usado para exponenciação, enquanto em C/C++, \n\tvocê usaria a função pow ou implementaria um loop para calcular a potência.'
-      '\n\t='+'=' *100)
+      '\n\t='+'=' *110)
 
-print('\n\n\tCálculo do Fatorial em Python:\n')
+pause()
+limpaTela()
+
+print('\t='+'=' *30)
+print('\tCálculo do Fatorial em Python:\n')
 
 resultFatorial = 1
 
@@ -84,13 +111,21 @@ for i in range(1, 30 + 1):
 
 print('\n\tResultado do fatorial de 30 = {}'.format(resultFatorial))
 
-print('\n\t='+'=' *100)
+print('\t='+'=' *70)
+pause()
+limpaTela()
+
+print('\n\t='+'=' *120)
 print('\tEm C/C++, o tamanho do inteiro é geralmente de 4 bytes para int e 8 bytes para long long int. \n\n\tO valor máximo representável depende da arquitetura do sistema.'
       'um int de 4 bytes, o valor máximo seria 2^31 - 1 (2147483647). \n\tSe considerarmos um long long int de 8 bytes, o valor máximo seria 2^63 - 1.'
       '\n\n\tO valor do fatorial de 30 em Python, que é um número significativamente grande, não teria problemas em ser representado em Python. \n\tPython automaticamente usa uma quantidade de memória suficiente para lidar com números grandes'
-      '\n\t='+'=' *100)
+      '\n\t='+'=' *120)
 
-print('\n\n\tImutabilidade Númerica em Python:')
+pause()
+limpaTela()
+
+print('\n\t='+'=' *50)
+print('\tImutabilidade Númerica em Python:')
 
 # Atribuição de um valor inteiro
 numImult_1 = 100
@@ -101,8 +136,11 @@ numImult_1 += 1
 print('\n\tApós a variavel numImult_1 ser incrementada: {}'
       '\n\tEnquanto a variavel numImult_2 continua igual a {}'.format(numImult_1, numImult_2))
 
-print('\n\t='+'=' *100)
+print('\n\t='+'=' *50)
+pause()
+limpaTela()
 
+print('\n\t='+'=' *80)
 print('\tMetodos disponiveis em variaveis inteiras:')
 
 print('\n\ttype(): Retorna o tipo do objeto: {}'.format(type(numImult_1)))
@@ -125,4 +163,6 @@ print('\tnumImult é um valor inteiro? {}'.format(numImult_1.is_integer()))
 numImult_1 = 100.05
 print('\tnumImult é um valor inteiro? {}'.format(numImult_1.is_integer()))
 
-print('\t='+'=' *100)
+print('\t='+'=' *80)
+pause()
+limpaTela() 
