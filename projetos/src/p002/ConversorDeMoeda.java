61b911581f06143643433bcebce45dc3177e3219
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2023-11-14 09:58:49 -0300

    Atividade P002: ConversorDeMoeda Finalizado

--- projetos/src/p002/ConversorDeMoeda.java
+++ projetos/src/p002/ConversorDeMoeda.java
@@ -3,6 +3,43 @@ Crie um programa que converta uma quantidade em dólares para outra moeda (por e
 Peça ao usuário que insira a taxa de câmbio e a quantidade em dólares. Em seguida, calcule e exiba o valor convertido.  */
 package p002;
 
+import java.util.Scanner;
+
 public class ConversorDeMoeda {
-  
+
+  public static void main(String[] args) {
+
+    Scanner scan = new Scanner(System.in);
+
+    limparTela();
+    System.out.println("\n\t==========CONVERSOR DE MOEDA==========");
+    System.out.print("\n\tInsira a taxa de câmbio para o dolar: ");
+    double taxaCambio = scan.nextDouble();
+
+    System.out.print("\n\tDigite a quantidade de dólares: ");
+    double quantidadeDolares = scan.nextDouble();
+
+    double reais = quantidadeDolares * taxaCambio;
+
+    System.out.printf("\n\tO valor em reais é: R$ %.2f", reais);
+    System.out.println("\n\t=======================================\n");
+
+    scan.close();
+  }
+
+  public static void limparTela() {
+    try {
+      final String os = System.getProperty("os.name");
+
+      if (os.contains("Windows")) {
+        new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+      } else {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+      }
+    } catch (final Exception e) {
+      // Trata exceções (pode ser uma exceção de interrupção)
+      e.printStackTrace();
+    }
+  }
 }
