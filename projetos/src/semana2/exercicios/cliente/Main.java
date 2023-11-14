bc2ab08b2e9ab4d6c39c872cfd58c05ab8bdaee0
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2023-11-14 19:13:31 -0300

    Classe Cliente atualizada

--- projetos/src/semana2/exercicios/cliente/Main.java
+++ projetos/src/semana2/exercicios/cliente/Main.java
@@ -16,18 +16,31 @@ public class Main {
         System.out.printf("\n\tInforme o cpf: ");
         String cpf = scan.nextLine();
 
-        Cliente cliente = new Cliente();
+        Cliente cliente = new Cliente(nome, cpf);
 
         limparTela();
         System.out.println("\t=========CLIENTE=========");
         System.out.printf("\tNome: %s\n\tcpf: %s", cliente.getNome(), cliente.getCpf());
-        System.out.println("\n\t====================");
+        System.out.println("\n\t=========================");
         pausa(scan);
 
         limparTela();
         cliente.setNome("Albert");
         System.out.println("\n\t=====NOME ALTERADO=====");
-        System.out.println("\n\tNome atual: "+ cliente.getNome() + "\n");
+        System.out.println("\tNome atual: "+ cliente.getNome() + "\n");
+        System.out.printf("\tAtributo idade inicializada com %d no construtor", cliente.getIdade());
+        System.out.println("\n\t====================");
+
+        limparTela();
+        System.out.println("\n\t====================");
+        System.out.printf("\n\tInforme a idade: ");
+        int idade = scan.nextInt();
+        cliente.setIdade(idade);
+
+        limparTela();
+        System.out.println("\n\t=====IDADE ALTERADA=====");
+        System.out.printf("\n\tA Idade atual de %s é %d.", cliente.getNome(), cliente.getIdade());
+        System.out.println("\n\t==============================");
 
         scan.close();
     }
