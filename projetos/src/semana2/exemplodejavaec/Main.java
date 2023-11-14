09f36f730cb8036ef08ddb34990a86307379d115
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2023-11-14 15:31:56 -0300

    Pacotes: Exemplojavaec e Exercicio Criada

---
+++ projetos/src/semana2/exemplodejavaec/Main.java
@@ -0,0 +1,12 @@
+package semana2.exemplodejavaec;
+
+public class Main {
+    
+    public static void main(String[] args){
+
+        Conta conta = new Conta();
+
+        conta.inicializa("Conta Corrente", 100);
+        System.out.println("\n\tNome: "+ conta.getNome());
+    }
+}
---
+++ projetos/src/semana2/exercicio/Cliente.java
@@ -0,0 +1,43 @@
+package semana2.exercicio;
+
+public class Cliente {
+    
+    private String nome;
+    private int idade;
+    private String cpf;
+    
+    public Cliente() {
+    }
+
+    public Cliente(String nome, int idade, String cpf) {
+        this.nome = nome;
+        this.idade = idade;
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    
+}
