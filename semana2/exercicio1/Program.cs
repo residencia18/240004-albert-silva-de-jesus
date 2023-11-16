75885e873e8c2da8b25943c6112979695fdc3bad
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2023-11-16 15:55:51 -0300

    Exercicio 1 e 2 Resolvido

---
+++ semana2/exercicio1/Program.cs
@@ -0,0 +1,71 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Globalization;
+
+namespace exercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LimparTela();
+            Console.WriteLine("\n\t========== EXERCICIO 1 ==========");
+            for (int i = 0; i < 30; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    Console.WriteLine("\tDivisiveis por 3, entre 0 e 30: " + i);
+                }
+            }
+            Console.WriteLine("\t==================================");
+            Pause();
+
+            LimparTela();
+            Console.WriteLine("\n\t========== EXERCICIO 1 ==========");
+            for (int i = 0; i < 30; i++)
+            {
+                if (i % 4 == 0)
+                {
+                    Console.WriteLine("\tDivisiveis por 4, entre 0 e 30: " + i);
+                }
+            }
+            Console.WriteLine("\t==================================");
+            Pause();
+
+            LimparTela();
+            Console.WriteLine("\n\t========== EXERCICIO 2 ==========");
+
+            string serieFibonacci = "";
+            int anterior = 0;
+            int atual = 1;
+
+            while (atual <= 100)
+            {
+                serieFibonacci += atual + " ";
+                int proximo = anterior + atual;
+                anterior = atual;
+                atual = proximo;
+            }
+            Console.WriteLine("A série de Fibonacci até 100: " + serieFibonacci);
+
+        }
+        static void LimparTela()
+        {
+            // Limpar a tela no Windows ou Linux
+            if (Environment.OSVersion.Platform == PlatformID.Win32NT)
+            {
+                Console.Clear(); // Windows
+            }
+            else
+            {
+                Console.Write("\u001b[2J\u001b[1;1H"); // Linux
+            }
+        }
+
+        static void Pause()
+        {
+            Console.Write("\n\tPressione Enter para continuar...");
+            Console.ReadLine();
+        }
+    }
+}
