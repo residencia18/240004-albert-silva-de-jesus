e44f89498d14c686802bc1c835b2148df79c09a3
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2023-11-25 21:54:31 -0300

    Exercicios de Exceção Finalizado

--- semana3/exercicios/Program.cs
+++ semana3/exercicios/Program.cs
@@ -102,6 +102,100 @@ namespace exercicios
       // Exibindo o resultado
       Console.WriteLine($"\n\n\tAltura média das pessoas: {alturaMedia} cm");
 
+
+      /*Crie um programa que solicita ao usuário que insira um número. Utilize um bloco try-catch para lidar com a possível exceção 
+      gerada se o usuário inserir algo que não seja um número. No bloco catch, exiba uma mensagem amigável informando ao usuário que um 
+      número válido deve ser inserido.*/
+
+      Console.Write("\n\tDigite um número: ");
+
+      try
+      {
+        int numero = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine($"\tVocê inseriu o número: {numero}");
+      }
+      catch (FormatException)
+      {
+        // Captura a exceção FormatException (ocorrida quando o usuário insere algo que não é um número)
+        Console.WriteLine("\tPor favor, insira um número válido.");
+      }
+
+      /*Escreva uma função que recebe dois números como parâmetros e realiza uma divisão. No entanto, utilize um bloco try-catch para 
+      lidar com a exceção de divisão por zero. Se uma divisão por zero for detectada, exiba uma mensagem indicando que a divisão por zero
+      não é permitida.*/
+
+      try
+      {
+        double resultado = RealizarDivisao(10, 0);
+        Console.WriteLine($"\tResultado da divisão: {resultado}");
+      }
+      catch (DivideByZeroException)
+      {
+        Console.WriteLine("\tDivisão por zero não é permitida.");
+      }
+
+      /*Modifique o exercício 2 para incluir um bloco finally. No bloco finally, exiba uma mensagem indicando que o bloco finally 
+      foi alcançado, independentemente de ocorrer uma exceção ou não.*/
+      try
+      {
+        // Chamando a expressão lambda com diferentes valores
+        double result1 = CalcularSomaDosQuadrados(2, 3);
+        double result2 = CalcularSomaDosQuadrados(4, 5);
+
+        // Exibindo os resultados
+        Console.WriteLine($"\n\tResultado 1: {result1}");
+        Console.WriteLine($"\tResultado 2: {result2}");
+      }
+      catch (Exception ex)
+      {
+        Console.WriteLine($"\tOcorreu uma exceção: {ex.Message}");
+      }
+      finally
+      {
+        Console.WriteLine("\tBloco finally foi alcançado.");
+      }
+
+      /*Crie uma função que simule uma operação que requer um número positivo como entrada. Se a função receber um número negativo, 
+      lance uma exceção personalizada indicando que números negativos não são permitidos. Utilize um bloco try-catch para lidar com 
+      essa exceção e exiba uma mensagem apropriada.*/
+      try
+      {
+        // Chamando a função com diferentes valores
+        RealizarOperacao(5);   // Isso deve ser bem-sucedido
+        RealizarOperacao(-2);  // Isso deve gerar uma exceção
+      }
+      catch (NumeroNegativoException ex)
+      {
+        Console.WriteLine($"\tErro: {ex.Message}");
+      }
+
+      /*Escreva um programa que lê um arquivo de texto a partir de um caminho fornecido pelo usuário. Utilize um bloco try-catch para lidar 
+      com exceções que possam ocorrer, como FileNotFoundException ou IOException. Exiba mensagens específicas para cada tipo de exceção 
+      capturada.*/
+
+      Console.Write("\n\tInforme o caminho do arquivo de texto: ");
+      string caminhoArquivo = Console.ReadLine()!;
+
+      try
+      {
+        // Tenta ler o conteúdo do arquivo
+        string conteudo = LerArquivo(caminhoArquivo);
+        Console.WriteLine($"\n\tConteúdo do arquivo:\n{conteudo}");
+      }
+      catch (FileNotFoundException)
+      {
+        Console.WriteLine($"\n\tErro: O arquivo não foi encontrado no caminho '{caminhoArquivo}'.");
+      }
+      catch (IOException ex)
+      {
+        Console.WriteLine($"\n\tErro de I/O: {ex.Message}");
+      }
+      catch (Exception ex)
+      {
+        Console.WriteLine($"\n\tErro inesperado: {ex.Message}");
+      }
+
     }
 
     static (string Nome, int Idade) CriarTupla(string nome, int idade)
@@ -122,6 +216,61 @@ namespace exercicios
     {
       // Chama a expressão lambda
       return SomaDosQuadrados(numero1, numero2);
+    }
+
+    static void RealizarOperacao(int numero)
+    {
+      try
+      {
+        if (numero < 0)
+        {
+          // Lança uma exceção personalizada se o número for negativo
+          throw new NumeroNegativoException("\n\tNúmeros negativos não são permitidos.");
+        }
+
+        // Continua com a operação se o número for positivo
+        Console.WriteLine($"\tOperação realizada com sucesso para o número {numero}");
+      }
+      catch (NumeroNegativoException ex)
+      {
+        // Captura e trata a exceção personalizada
+        throw ex; // Você pode optar por relançar a exceção ou tratar de outra forma
+      }
+    }
+
+    static double RealizarDivisao(double numerador, double denominador)
+    {
+      if (denominador == 0)
+      {
+        throw new DivideByZeroException("Divisão por zero não é permitida.");
+      }
+
+      // Realiza a divisão
+      return numerador / denominador;
+    }
+
+    static double CalcularSomaDosQuadrados2(double numero1, double numero2)
+    {
+      try
+      {
+        // Chama a expressão lambda
+        return SomaDosQuadrados(numero1, numero2);
+      }
+      catch (Exception ex)
+      {
+        // Captura exceção e re-lança
+        throw new Exception("Ocorreu uma exceção durante o cálculo.", ex);
+      }
+      finally
+      {
+        Console.WriteLine("Bloco finally interno foi alcançado.");
+      }
+    }
+
+    static string LerArquivo(string caminho)
+    {
+      // Tenta ler o conteúdo do arquivo e retorna uma string
+      return File.ReadAllText(caminho);
     }
 
     public static void LimparTela()
