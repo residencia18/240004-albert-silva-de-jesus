12c1cebebdb4860310a0e56a5b5989e018708e76
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2023-11-30 17:59:03 -0300

    Advogado Parcialmente Concluido

--- AvaliacaoDotNet/Advogado.cs
+++ AvaliacaoDotNet/Advogado.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Globalization;
 
 namespace AvaliacaoDotNet
 {
@@ -10,11 +11,149 @@ namespace AvaliacaoDotNet
         public string Cna { get; set; }
         public string Especialidade { get; set; }
 
-        public Advogado(string nome, DateTime dataNascimento, int idade, string cpf, string cna, string especialidade) 
+        public Advogado(string nome, DateTime dataNascimento, int idade, string cpf, string cna, string especialidade)
         : base(nome, dataNascimento, cpf, idade)
         {
             Cna = cna;
             Especialidade = especialidade;
         }
+
+        public static bool IsCpfUnico(string cpf, List<Advogado> advogados)
+        {
+            // Verifica se o CPF é válido antes de proceder
+            if (!IsValidCPF(cpf))
+            {
+                throw new ArgumentException("\n\tOps, CPF inválido!...");
+            }
+
+            // Verifica se o CPF já existe na lista de pacientes
+            return !advogados.Any(advogado => advogado.Cpf == cpf);
+        }
+
+        public static bool IsValidCPF(string cpf)
+        {
+            // Remover caracteres não numéricos
+            string numbersOnly = new string(cpf.Where(char.IsDigit).ToArray());
+
+            // Verificar se o CPF possui 11 dígitos
+            if (numbersOnly.Length != 11)
+            {
+                return false;
+            }
+
+            // Calcular os dígitos verificadores
+            int[] cpfDigits = numbersOnly.Select(c => int.Parse(c.ToString())).ToArray();
+            int sum = 0;
+
+            for (int i = 0; i < 9; i++)
+            {
+                sum += cpfDigits[i] * (10 - i);
+            }
+
+            int firstDigit = 11 - (sum % 11);
+            if (firstDigit > 9)
+            {
+                firstDigit = 0;
+            }
+
+            sum = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                sum += cpfDigits[i] * (11 - i);
+            }
+
+            int secondDigit = 11 - (sum % 11);
+            if (secondDigit > 9)
+            {
+                secondDigit = 0;
+            }
+
+            // Verifica se os dígitos calculados correspondem aos dígitos informados no CPF
+            return cpfDigits[9] == firstDigit && cpfDigits[10] == secondDigit;
+        }
+        public static string ConvertePrimeiraLetraParaMaiuscula(string palavra)
+        {
+            string palavraComMaiuscula = "";
+            bool converteu = true;
+
+            do
+            {
+                // Verifica se a string não está vazia
+                if (!string.IsNullOrEmpty(palavra))
+                {
+                    // Verifica se todos os caracteres na palavra são letras
+                    if (palavra.All(char.IsLetter))
+                    {
+                        // Converte a primeira letra para maiúscula
+                        palavraComMaiuscula = char.ToUpper(palavra[0]) + palavra.Substring(1);
+                        converteu = false;
+                    }
+                    else
+                    {
+                        Console.WriteLine("\n\tA string deve conter apenas letras.");
+                        Console.Write("\tPressione Enter para continuar... ");
+                        Console.ReadLine();
+                        App.LimparTela();
+
+                        Console.Write("\n\tInforme o nome do Paciente: ");
+                        palavra = Console.ReadLine()!;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("\n\tA string está vazia.");
+                    Console.Write("\tPressione Enter para continuar... ");
+                    Console.ReadLine();
+                    App.LimparTela();
+
+                    Console.Write("\n\tInforme o nome do Paciente: ");
+                    palavra = Console.ReadLine()!;
+                }
+            } while (converteu);
+
+            return palavraComMaiuscula;
+        }
+
+        public static DateTime ObterDataDeNascimento()
+        {
+            string inputDataNascimento;
+            DateTime dataNascimento;
+
+            do
+            {
+                Console.Write("\n\tDigite a data de nascimento do paciente (no formato dd/MM/yyyy): ");
+                inputDataNascimento = Console.ReadLine()!;
+
+            } while (!TentarObterDataValida(inputDataNascimento, out dataNascimento));
+
+            return dataNascimento;
+        }
+
+        private int CalcularIdade(DateTime dataNascimento)
+        {
+            int idade = DateTime.Now.Year - dataNascimento.Year;
+
+            // Verificar se o aniversário já ocorreu neste ano
+            if (dataNascimento.Date > DateTime.Now.Date.AddYears(-idade))
+            {
+                idade--;
+            }
+
+            return idade;
+        }
+
+        public static bool TentarObterDataValida(string input, out DateTime data)
+        {
+            if (DateTime.TryParseExact(input, "dd/MM/yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out data)
+                && data.Date <= DateTime.Now.Date)
+            {
+                return true;
+            }
+
+            // Se a conversão falhar ou a data for no futuro, retorne false
+            Console.WriteLine("\n\tFormato de data inválido ou data no futuro. Digite novamente.");
+            return false;
+        }
+
     }
 }
--- AvaliacaoDotNet/ListaAdvogado.cs
+++ AvaliacaoDotNet/ListaAdvogado.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 
 namespace AvaliacaoDotNet
@@ -14,7 +15,7 @@ namespace AvaliacaoDotNet
             advogados = new List<Advogado>();
         }
 
-        public void AdicionarCliente(Advogado advogado)
+        public void AdicionarAdvogado(Advogado advogado)
         {
             advogados.Add(advogado);
         }
@@ -22,6 +23,53 @@ namespace AvaliacaoDotNet
         public List<Advogado> GetAdvogados()
         {
             return advogados;
+        }
+
+         public void Cadastrar()
+        {
+            Console.Write("\n\tDigite o nome do advogado: ");
+            string nome = Console.ReadLine()!;
+            nome = Cliente.ConvertePrimeiraLetraParaMaiuscula(nome);
+
+            string cpf;
+            do
+            {
+                Console.Write("\n\tDigite o CPF do advogado: ");
+                cpf = Console.ReadLine()!;
+
+                if (!Advogado.IsValidCPF(cpf) || !Advogado.IsCpfUnico(cpf, advogados))
+                {
+                    Console.WriteLine("\n\tOps, CPF inválido ou já existe no cadastro. Por favor, digite um CPF válido.");
+                    App.Pause();
+                }
+
+            } while (!Advogado.IsValidCPF(cpf) || !Advogado.IsCpfUnico(cpf, advogados));
+
+            DateTime dataNascimento = Advogado.ObterDataDeNascimento();
+
+            Console.Write("\n\tDigite o CNA do advogado: ");
+            string cna = Console.ReadLine()!;
+
+            Console.Write("\n\tDigite a especialidade do advogado: ");
+            string especialidades = Console.ReadLine()!;
+
+            AdicionarAdvogado(new Advogado(nome, dataNascimento, 43, cpf, cna, especialidades));
+
+        }
+
+        public void Listar()
+        {
+            Console.WriteLine("\n\t=== Lista de Pacientes ===");
+            foreach (Advogado advogado in advogados)
+            {
+                Console.WriteLine("\tNome: " + advogado.Nome);
+                Console.WriteLine("\tCPF: " + advogado.Cpf);
+                Console.WriteLine("\tData de Nascimento: " + advogado.DataNascimento.ToString("dd/MM/yyyy"));
+                Console.WriteLine("\tIdade: " + advogado.Idade);
+                Console.WriteLine("\tCNA: " + advogado.Cna);
+                Console.WriteLine("\tEspecialidade: " + advogado.Especialidade);
+                Console.WriteLine("\t==========================\n");
+            }
         }
     }
 }
