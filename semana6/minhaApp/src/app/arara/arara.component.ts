b71b6eb9659a6bd261478e5ef517d9816e6411f6
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2023-12-13 16:35:04 -0300

    Projeto Angula e P005

---
+++ semana6/minhaApp/src/app/arara/arara.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-arara',
+  templateUrl: './arara.component.html',
+  styleUrl: './arara.component.css'
+})
+export class AraraComponent {
+
+  nome:string = 'Arara';
+}
