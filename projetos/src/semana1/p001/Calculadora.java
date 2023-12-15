1acf2affef6f3eb4bc4160b87ac5ea28cb873d67
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2023-12-15 17:00:31 -0300

    Pakages Atualizados

--- projetos/src/semana1/p001/Calculadora.java
+++ projetos/src/semana1/p001/Calculadora.java
@@ -98,8 +98,6 @@ public class Calculadora {
           break;
       }
 
-      
-
     }
 
   }
--- projetos/src/semana5/atvEmSala/exDegas/poligonos/Poligono.java
+++ projetos/src/semana5/atvEmSala/exDegas/poligonos/Poligono.java
@@ -3,9 +3,9 @@ package poligonos;
 import java.util.ArrayList;
 
 public class Poligono {
-	
+
 	private ArrayList<Ponto> pontos;
-	
+
 	Poligono() {
 		pontos = new ArrayList<Ponto>();
 	}
@@ -15,16 +15,16 @@ public class Poligono {
 	}
 
 	public void mostraPontos() {
-		for (Ponto p: pontos)
+		for (Ponto p : pontos)
 			p.mostra();
 	}
 
 	public float perimetro() {
 		float per = 0;
-		Ponto p1, p2=pontos.get(1);
-		for (int i=0; i< pontos.size()-1; i++) {
+		Ponto p1, p2 = pontos.get(1);
+		for (int i = 0; i < pontos.size() - 1; i++) {
 			p1 = pontos.get(i);
-			p2 = pontos.get(i+1);
+			p2 = pontos.get(i + 1);
 			per += p1.distancia(p2);
 		}
 		p1 = pontos.get(0);
