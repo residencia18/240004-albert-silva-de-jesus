e56051737d68dc4ba7f265ebbb53af05159f3bb2
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2023-12-18 08:43:28 -0300

    sobrecarga

--- projetos/src/semana5/atvEmSala/sobrecarca/Main.java
+++ projetos/src/semana5/atvEmSala/sobrecarca/Main.java
@@ -6,36 +6,45 @@ listar (int x, int y) → lista todos os usuários entre as posições x e y (me
 package semana5.atvEmSala.sobrecarca;
 
 public class Main {
-    
+
     public static void main(String[] args) throws Exception {
 
-        Usuarios usuario1 = new Usuarios("João", "123.456.789-00");
-        Usuarios usuario2 = new Usuarios("Maria", "987.654.321-00");
-        Usuarios usuario3 = new Usuarios("José", "456.789.123-00");
-        Usuarios usuario4 = new Usuarios("Ana", "654.321.987-00");
-        Usuarios usuario5 = new Usuarios("Carlos", "321.654.987-00");
-        Usuarios usuario6 = new Usuarios("Mariana", "789.123.456-00");
-        Usuarios usuario7 = new Usuarios("Pedro", "321.987.654-00");
-        Usuarios usuario8 = new Usuarios("Paula", "789.456.123-00");
-        Usuarios usuario9 = new Usuarios("Lucas", "987.321.654-00");
-        Usuarios usuario10 = new Usuarios("Luana", "456.789.321-00");
+        limparTela(); 
+        Usuarios usuario = new Usuarios();  
 
-        usuario1.cadastrarUsuario(usuario1);
-        usuario1.cadastrarUsuario(usuario2);
-        usuario1.cadastrarUsuario(usuario3);
-        usuario1.cadastrarUsuario(usuario4);
-        usuario1.cadastrarUsuario(usuario5);
-        usuario1.cadastrarUsuario(usuario6);
-        usuario1.cadastrarUsuario(usuario7);
-        usuario1.cadastrarUsuario(usuario8);
-        usuario1.cadastrarUsuario(usuario9);
-        usuario1.cadastrarUsuario(usuario10);
+        usuario.cadastrarUsuario(new Usuarios("João", "123.456.789-00"));
+        usuario.cadastrarUsuario(new Usuarios("José", "456.789.123-00"));
+        usuario.cadastrarUsuario(new Usuarios("Ana", "654.321.987-00"));
+        usuario.cadastrarUsuario(new Usuarios("Carlos", "321.654.987-00"));
+        usuario.cadastrarUsuario(new Usuarios("Mariana", "789.123.456-00"));
+        usuario.cadastrarUsuario(new Usuarios("Pedro", "321.987.654-00"));
+        usuario.cadastrarUsuario(new Usuarios("Paula", "789.456.123-00"));
+        usuario.cadastrarUsuario(new Usuarios("Lucas", "987.321.654-00"));
+        usuario.cadastrarUsuario(new Usuarios("Luana", "456.789.321-00"));
 
-        usuario1.listarUsuarios();
+        usuario.listarUsuarios();
         System.out.println("=====================================");
-        usuario1.listarUsuarios(5);
+        // usuario1.listarUsuarios(2);
         System.out.println("=====================================");
-        usuario1.listarUsuarios(5, 8);
+        usuario.listarUsuarios(2, 5);
+        System.out.println("=====================================");
+        usuario.listarUsuarios(3);
 
+    }
+
+    public static void limparTela() {
+        try {
+            final String os = System.getProperty("os.name");
+
+            if (os.contains("Windows")) {
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            } else {
+                System.out.print("\033[H\033[2J");
+                System.out.flush();
+            }
+        } catch (final Exception e) {
+            // Trata exceções (pode ser uma exceção de interrupção)
+            e.printStackTrace();
+        }
     }
 }
