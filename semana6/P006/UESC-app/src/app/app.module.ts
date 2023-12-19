2caf618bfa172dc5b01cd05878322497f4adc6a5
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2023-12-19 20:24:24 -0300

    Components Criados

--- semana6/P006/UESC-app/src/app/app.module.ts
+++ semana6/P006/UESC-app/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NoticiasComponent } from './noticias/noticias.component';
+import { DestaqueComponent } from './destaque/destaque.component';
+import { ServicoComponent } from './servico/servico.component';
+import { ResultadoComponent } from './resultado/resultado.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NoticiasComponent,
+    DestaqueComponent,
+    ServicoComponent,
+    ResultadoComponent
   ],
   imports: [
     BrowserModule,
