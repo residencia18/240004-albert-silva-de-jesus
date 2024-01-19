d0709e29b4a0d0b5535f8e0636d9dff38062ddfa
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2024-01-19 16:57:51 -0300

    componentes Criados

--- semana7/p007/src/app/app.module.ts
+++ semana7/p007/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ClassesComponent } from './componentes/classes/classes.component';
+import { ObjetosComponent } from './componentes/objetos/objetos.component';
+import { PropriedadesComponent } from './componentes/propriedades/propriedades.component';
+import { ValorPropriedadesComponent } from './componentes/valor-propriedades/valor-propriedades.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClassesComponent,
+    ObjetosComponent,
+    PropriedadesComponent,
+    ValorPropriedadesComponent
   ],
   imports: [
     BrowserModule,
---
+++ semana7/p007/src/app/componentes/valor-propriedades/valor-propriedades.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-valor-propriedades',
+  templateUrl: './valor-propriedades.component.html',
+  styleUrl: './valor-propriedades.component.css'
+})
+export class ValorPropriedadesComponent {
+
+}
