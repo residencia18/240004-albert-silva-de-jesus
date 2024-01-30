7a9aeaaf0a72cef14decd0e1cf1db40b7999192a
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2024-01-30 16:33:03 -0300

    Db.properties Program e DB

--- projetos/src/jdbc/redesocial/application/Program.java
+++ projetos/src/jdbc/redesocial/application/Program.java
@@ -10,16 +10,38 @@ public class Program {
 
   public static void main(String[] args) {
 
+    limparTela(); 
     Scanner sc = new Scanner(System.in);
 
     UsuarioDao usuarioDao = DaoFactory.createUsuarioDao();
 
     System.out.println("\n\t=== TEST 1: usuario insert =====");
-    Usuario usuario = new Usuario(null, "Tico", "90876", "tico@hotmail.com");
+    Usuario usuario = new Usuario(null, "street", "334455", "tacio@Yaho.com");
     usuarioDao.insert(usuario);
     System.out.println("\n\tInserted! New id: " + usuario.getId());
 
     sc.close();
 
+  }
+
+  public static void limparTela() {
+    try {
+      final String os = System.getProperty("os.name");
+
+      if (os.contains("Windows")) {
+        new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+      } else {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+      }
+    } catch (final Exception e) {
+      // Trata exceções (pode ser uma exceção de interrupção)
+      e.printStackTrace();
+    }
+  }
+
+  public static void pausar(Scanner scan) {
+    System.out.print("\n\tPressione ENTER para continuar...");
+    scan.nextLine();
   }
 }
--- projetos/src/jdbc/redesocial/db/DB.java
+++ projetos/src/jdbc/redesocial/db/DB.java
@@ -41,7 +41,7 @@ public class DB {
 
   private static Properties loadProperties() {
 
-    try (FileInputStream fs = new FileInputStream("db.properties")) {
+    try (FileInputStream fs = new FileInputStream("projetos\\src\\jdbc\\redesocial\\db.properties")) {
       Properties props = new Properties();
       props.load(fs);
       return props;
