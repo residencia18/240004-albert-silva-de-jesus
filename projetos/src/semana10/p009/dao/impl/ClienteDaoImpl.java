90c4286addb85583daca1b515bd99888f1a24167
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2024-02-01 22:42:38 -0300

    Crud cliente, ok

--- projetos/src/semana10/p009/dao/impl/ClienteDaoImpl.java
+++ projetos/src/semana10/p009/dao/impl/ClienteDaoImpl.java
@@ -88,62 +88,57 @@ public class ClienteDaoImpl implements ClienteDao {
     System.out.print("\n\tDigite o CPF do cliente: ");
     String cpf = Views.scan.nextLine();
 
-    for (Cliente cliente : clientes) {
+    Integer clienteId = findClientIdByCpf(cpf);
 
-      if (cliente.getCpf().equals(cpf)) {
+    if (clienteId != null) {
+      Cliente clienteParaEditar = findById(clienteId);
 
-        Views.limparTela();
-        System.out.print("\n\t===== DADOS DO CLIENTE =====");
-        System.out.println(cliente.toString());
+      Views.limparTela();
+      System.out.print("\n\t===== DADOS DO CLIENTE =====");
+      System.out.println(clienteParaEditar.toString());
 
-        while (true) {
+      try {
+        System.out.print("\n\tDeseja realmente editar este cliente? (S/N): ");
+        String opcao = Views.scan.nextLine();
 
-          try {
-            System.out.print("\n\tDeseja realmente editar este cliente? (S/N): ");
-            String opcao = Views.scan.nextLine();
-
-            if (!opcao.equalsIgnoreCase("s") && !opcao.equalsIgnoreCase("n")) {
-              throw new InputMismatchException("Opção inválida. Digite 'S' para confirmar ou 'N' para cancelar.");
-            }
-
-            if (opcao.equalsIgnoreCase("s")) {
-
-              Views.limparTela();
-              System.out.print("\n\tDigite o novo nome do cliente: ");
-              String nome = Views.scan.nextLine();
-
-              System.out.println("\n\tDigite o novo CPF: ");
-              cpf = Views.scan.nextLine();
-
-              cliente.setNome(nome);
-              cliente.setCpf(cpf);
-
-              update(cliente);
-
-              Views.limparTela();
-              System.out.print("\n\t===== CLIENTE EDITADO =====");
-
-              System.out.println(cliente.toString());
-              System.out.println("\n\tCliente editado com sucesso!");
-              Views.pausar(Views.scan);
-              return;
-
-            } else {
-              System.out.println("\n\tOperação cancelada!");
-              Views.pausar(Views.scan);
-              return;
-            }
-          } catch (InputMismatchException e) {
-            System.out.println("\n\t" + e.getMessage());
-            // Limpar o buffer do scanner antes de continuar o loop
-            Views.scan.nextLine();
-          }
+        if (!opcao.equalsIgnoreCase("s") && !opcao.equalsIgnoreCase("n")) {
+          throw new InputMismatchException("Opção inválida. Digite 'S' para confirmar ou 'N' para cancelar.");
         }
+
+        if (opcao.equalsIgnoreCase("s")) {
+          Views.limparTela();
+          System.out.print("\n\tDigite o novo nome do cliente: ");
+          String nome = Views.scan.nextLine();
+
+          System.out.print("\n\tDigite o novo CPF: ");
+          cpf = Views.scan.nextLine();
+
+          clienteParaEditar.setNome(nome);
+          clienteParaEditar.setCpf(cpf);
+
+          update(clienteParaEditar);
+
+          Views.limparTela();
+          System.out.print("\n\t===== CLIENTE EDITADO =====");
+
+          System.out.println(clienteParaEditar.toString());
+          System.out.println("\n\tCliente editado com sucesso!");
+          Views.pausar(Views.scan);
+          return;
+
+        } else {
+          System.out.println("\n\tOperação cancelada!");
+          Views.pausar(Views.scan);
+          return;
+        }
+      } catch (InputMismatchException e) {
+        System.out.println("\n\t" + e.getMessage());
+        Views.scan.nextLine(); // Limpar o buffer do scanner antes de continuar o loop
       }
+    } else {
+      System.out.println("\n\tOps, Cliente não encontrado!..");
+      Views.pausar(Views.scan);
     }
-    System.out.println("\n\tOps, Cliente não encontrado!..");
-    Views.pausar(Views.scan);
-    return;
   }
 
   @Override
@@ -165,7 +160,7 @@ public class ClienteDaoImpl implements ClienteDao {
       System.out.println(clienteParaRemover.toString());
 
       try {
-        
+
         System.out.print("\n\tDeseja realmente excluir este cliente? (S/N): ");
         String opcao = Views.scan.nextLine();
 
