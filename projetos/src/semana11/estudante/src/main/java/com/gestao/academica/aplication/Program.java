061b6d275cce7ffe3f0986549b21568a06be3c14
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2024-02-06 15:46:36 -0300

    Método listarTodos

--- projetos/src/semana11/estudante/src/main/java/com/gestao/academica/aplication/Program.java
+++ projetos/src/semana11/estudante/src/main/java/com/gestao/academica/aplication/Program.java
@@ -1,5 +1,7 @@
 package com.gestao.academica.aplication;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -14,19 +16,20 @@ public class Program {
     EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
     EntityManager em = emf.createEntityManager();
 
-    Curso c1 = new Curso(null, "Segurança da Informação", 6);
-    Estudante p1 = new Estudante(null, "Huelbert Lima", "huelbert@gmail.com",
-    "098765", c1);
-    // Pessoa p2 = new Pessoa(null, "Joaquim Torres", "joaquim@gmail.com");
-    // Pessoa p3 = new Pessoa(null, "Maria Joaquina", "maria@gmail.com");
+    // Curso c1 = new Curso(null, "Arquitetura de Software", 6);
+    // Curso c2 = new Curso(null, "Engenharia de Software", 7);
+    // Estudante p1 = new Estudante(null, "Alex Oliveira", "alex@gmail.com", "191817", c1);
+    // Estudante p2 = new Estudante(null, "Maria Joaquina", "maria@gmail.com", "345678", c2);
 
-    // Inserir dados
-    em.getTransaction().begin();
-    em.persist(c1);
-    em.persist(p1);
+    // // Inserir dados
+    // em.getTransaction().begin();
+    // em.persist(c1);
+    // em.persist(c2);
+    // em.persist(p1);
     // em.persist(p2);
-    // em.persist(p3);
-    em.getTransaction().commit();
+    // em.getTransaction().commit();
+
+    listarTodos(em).forEach(System.out::println);
 
     // Buscar por Id
 
@@ -41,5 +44,9 @@ public class Program {
     System.out.println("Pronto!...");
     emf.close();
     em.close();
+  }
+
+  public static List<Estudante> listarTodos(EntityManager entityManager) {
+    return entityManager.createQuery("SELECT e FROM Estudante e", Estudante.class).getResultList();
   }
 }
