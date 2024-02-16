38575315ff7f0d76d73620861a5560244abd86f2
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2024-02-16 00:33:54 -0300

    Lista de Pilotos

--- projetos/src/semana13/pilotos/src/main/java/com/pratica12/pilotos/controllers/PilotoController.java
+++ projetos/src/semana13/pilotos/src/main/java/com/pratica12/pilotos/controllers/PilotoController.java
@@ -1,13 +1,23 @@
 package com.pratica12.pilotos.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.pratica12.pilotos.services.PilotoService;
+import org.springframework.web.bind.annotation.GetMapping;
+
 @RestController
+@RequestMapping("/pilotos")
 public class PilotoController {
   
-  @RequestMapping("/hello") 
-  public String getHello() {
-    return "Hello, Piloto!";
+  @Autowired
+  private PilotoService pilotoService;
+
+  @GetMapping("/todos")
+  public String listarTodos() {
+    return pilotoService.getPilotos().toString();
   }
+  
+  
 }
