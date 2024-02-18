fd91ebae2e9477669129b278afddd5b7be587382
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2024-02-18 17:29:26 -0300

    Usuario

---
+++ projetos/src/semana14/pratica13/src/main/java/com/persistencia/pratica13/entities/Usuario.java
@@ -0,0 +1,21 @@
+package com.persistencia.pratica13.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Usuario {
+  
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  private String nome;
+  private String email;
+  private String senha;
+  private String telefone;
+  private String endereco;
+
+  
+}
