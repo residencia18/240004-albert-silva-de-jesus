18e6869e4e1fbd1ced6ba20764afcd65e3760108
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2024-02-18 17:21:54 -0300

    Projeto Inicializado

---
+++ projetos/src/semana14/pratica13/src/test/java/com/persistencia/pratica13/Pratica13ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.persistencia.pratica13;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pratica13ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
