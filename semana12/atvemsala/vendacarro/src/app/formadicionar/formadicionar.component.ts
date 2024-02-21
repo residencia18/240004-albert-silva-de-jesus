0365dcd22001f85963ff6b8f018a3a76641efebe
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2024-02-21 16:56:23 -0300

    Projeto rodando no Firebase

--- semana12/atvemsala/vendacarro/src/app/formadicionar/formadicionar.component.ts
+++ semana12/atvemsala/vendacarro/src/app/formadicionar/formadicionar.component.ts
@@ -1,23 +1,17 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ValidationErrors, Validators } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, ValidationErrors, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-formadicionar',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, NgIf],
   templateUrl: './formadicionar.component.html',
   styleUrl: './formadicionar.component.css'
 })
 export class FormadicionarComponent {
 
   carroFormulario: FormGroup;
-
-  private modelo: string = "Fusca";
-  private marca: string = "Volkswagen";
-  private ano: number = 1970;
-  private valor: number = 10000;
-  private cor: string = "Azul";
-  private foto: string = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.automaistv.com.br%2Fconheca-o-vw-fusca-moderno-que-custa-mais-que-uma-ferrari%2F&psig=AOvVaw0MoRAkmDld2LHJo9YIucHU&ust=1708625885850000&source=images&cd=vfe&opi=89978449&ved=0CBAQjRxqFwoTCLjH_8-FvYQDFQAAAAAdAAAAABAD";
 
   constructor() {
     this.carroFormulario = new FormGroup({
@@ -26,7 +20,7 @@ export class FormadicionarComponent {
       'ano': new FormControl(null, [Validators.required, Validators.minLength(4)]),
       'valor': new FormControl(null, [Validators.required, Validators.pattern(/^\(\d{2}\)\d{5}-\d{4}$/)]),
       'cor': new FormControl(null, [Validators.required]),
-      'foto': new FormControl(this.foto),
+      // 'foto': new FormControl(this.foto),
     });
   }
 
@@ -41,5 +35,15 @@ export class FormadicionarComponent {
       return { 'nomeInvalido': true };
     }
     return null;
+  }
+
+  enviaFormulario() {
+    const veiculoJson = {
+      modelo: this.carroFormulario.get('modelo')?.value,
+      marca: this.carroFormulario.get('marca')?.value,
+      ano: this.carroFormulario.get('ano')?.value,
+      valor: this.carroFormulario.get('valor')?.value,
+    }
+    console.log("dados do veiculo", veiculoJson)
   }
 }
