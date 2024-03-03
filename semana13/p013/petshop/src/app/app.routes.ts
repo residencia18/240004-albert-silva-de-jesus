c0212d9f35c95db8ae6f48bb6c5f645e3da1b41e
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2024-03-03 19:52:00 -0300

    Nav-Bar

--- semana13/p013/petshop/src/app/app.routes.ts
+++ semana13/p013/petshop/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { CreateComponent } from './components/create/create.component';
+import { FindAllComponent } from './components/find-all/find-all.component';
+import { UpdateComponent } from './components/update/update.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: CreateComponent },
+  { path: 'findAll', component: FindAllComponent },
+  { path: 'update/:id', component: UpdateComponent },
+
+];
---
+++ semana13/p013/petshop/src/app/components/find-all/find-all.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-find-all',
+  standalone: true,
+  imports: [],
+  templateUrl: './find-all.component.html',
+  styleUrl: './find-all.component.css'
+})
+export class FindAllComponent {
+
+}
