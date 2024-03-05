0fbc4f7db4da168293f312b7a2f572454d08df10
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2024-03-05 16:58:00 -0300

    auth

--- semana13/p013/petshop/src/app/app.routes.ts
+++ semana13/p013/petshop/src/app/app.routes.ts
@@ -2,9 +2,10 @@ import { Routes } from '@angular/router';
 import { CreateComponent } from './components/create/create.component';
 import { FindAllComponent } from './components/find-all/find-all.component';
 import { UpdateComponent } from './components/update/update.component';
+import { AuthComponent } from './auth/auth/auth.component';
 
 export const routes: Routes = [
-  { path: '', component: CreateComponent },
+  { path: 'autentica', component: AuthComponent },
   { path: 'create', component: CreateComponent },
   { path: 'findAll', component: FindAllComponent },
   { path: 'update/:id', component: UpdateComponent },
--- semana13/p013/petshop/src/app/auth/auth/auth.component.ts
+++ semana13/p013/petshop/src/app/auth/auth/auth.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AutenticaService } from '../../services/autentica.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
@@ -9,4 +12,64 @@ import { Component } from '@angular/core';
 })
 export class AuthComponent {
 
+  modoLogin = true;
+  estaCarregando = false;
+  erro: string = '';
+  temErro: boolean = false;
+
+  constructor(private authService: AutenticaService,
+    private router: Router) { }
+
+  onTrocarModo() {
+    this.modoLogin = !this.modoLogin;
+
+  }
+
+  onSubmit(formulario: NgForm) {
+    if (!formulario.valid) {
+      return;
+    }
+    const email = formulario.value.email;
+    const password = formulario.value.password;
+
+    this.estaCarregando = true;
+
+    if (this.modoLogin) {
+      this.authService.loginUser(email, password).subscribe(
+        responseData => {
+          console.log(responseData);
+          this.estaCarregando = false;
+          this.temErro = false;
+          this.router.navigate(['/pecas']);
+        }
+      );
+    } else {
+      this.authService.signupUser(email, password).subscribe(
+        responseData => {
+          console.log(responseData);
+          this.estaCarregando = false;
+          this.temErro = false;
+          this.router.navigate(['/pecas']);
+        },
+        error => {
+          console.log(error);
+          switch (error.error.error.message) {
+            case 'EMAIL_EXISTS':
+              this.erro = 'O e-mail já está em uso.';
+              break;
+            default:
+              this.erro = 'Ocorreu um erro ao cadastrar o usuário.'
+              break;
+
+          }
+          this.temErro = true;
+          this.erro = 'Ocorreu um erro ao cadastrar o usuário.'
+          this.estaCarregando = false;
+        }
+      );
+    }
+
+    formulario.reset();
+
+  }
 }
