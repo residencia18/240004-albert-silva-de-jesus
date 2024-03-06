983fb140862c354d81adedc45a34121367e52aa1
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2024-03-06 20:16:20 -0300

    Autenticação,ok

--- semana13/p013/petshop/src/app/app.component.ts
+++ semana13/p013/petshop/src/app/app.component.ts
@@ -2,8 +2,10 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { CrudService } from './services/crud.service';
+import { AuthService } from './services/auth.service';
+import { AuthInterceptor } from './interceptors/auth.interceptor';
 
 @Component({
   selector: 'app-root',
@@ -11,7 +13,7 @@ import { CrudService } from './services/crud.service';
   imports: [CommonModule, RouterOutlet, NavBarComponent, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  providers: [CrudService]
+  providers: [CrudService, AuthService, {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true}]
 })
 
 export class AppComponent {
