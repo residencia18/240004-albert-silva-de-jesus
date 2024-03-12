898342a1094286aa8b0584c4f622d0ba1b4fa27b
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2024-03-11 23:24:22 -0300

    UserService, UserForm, UserMapper, UserResponseDto e TestConfig

--- projetos/src/semana1/crud_user/src/main/java/com/example/crud_user/web/dto/form/UserForm.java
+++ projetos/src/semana1/crud_user/src/main/java/com/example/crud_user/web/dto/form/UserForm.java
@@ -1,23 +1,20 @@
-package com.example.crud_user.entities;
+package com.example.crud_user.web.dto.form;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Getter
-@Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@Entity
-@Table(name = "tb_user")
-public class User extends AbstractEntity{
+@Getter
+@Setter
+public class UserForm {
   
   private String name;
   private String email;
   private String cpf;
   private String phone;
   private String password;
+
 }
