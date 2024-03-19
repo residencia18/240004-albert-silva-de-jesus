651ee5914d411d081cf41c9f1dc4957e91a97c21
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2024-03-19 11:35:23 -0300

    Empoloyee e Configurações properties

--- java-avancado/aula-4/sell-genius/src/main/java/com/swproject/sellgenius/entities/Employee.java
+++ java-avancado/aula-4/sell-genius/src/main/java/com/swproject/sellgenius/entities/Employee.java
@@ -1,10 +1,35 @@
 package com.swproject.sellgenius.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import java.time.Instant;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "tb_employee")
-public class Employee extends AbstractEntity{
-  
+public class Employee extends AbstractEntity {
+
+  private String name;
+  @Column(unique = true)
+  private String cpf;
+  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
+  private Instant birthDate;
+
+  @ManyToOne
+  @JoinColumn(name = "usuario_id")
+  private User usuario;
+
 }
