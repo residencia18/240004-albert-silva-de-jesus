59e7d6e7e72522750ab09609eb544c19499c2120
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2024-03-19 11:26:29 -0300

    TestConfig e Product

--- java-avancado/aula-4/sell-genius/src/main/java/com/swproject/sellgenius/config/TestConfig.java
+++ java-avancado/aula-4/sell-genius/src/main/java/com/swproject/sellgenius/config/TestConfig.java
@@ -8,8 +8,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.swproject.sellgenius.entities.Category;
+import com.swproject.sellgenius.entities.Product;
 import com.swproject.sellgenius.entities.User;
 import com.swproject.sellgenius.repositories.CategoryRepository;
+import com.swproject.sellgenius.repositories.ProductRepository;
 import com.swproject.sellgenius.repositories.UserRepository;
 
 @Configuration
@@ -21,6 +23,9 @@ public class TestConfig implements CommandLineRunner {
 
   @Autowired
   private CategoryRepository categoryRepositoy;
+
+  @Autowired
+  private ProductRepository productRepository;
 
   @SuppressWarnings("null")
   @Override
@@ -39,6 +44,14 @@ public class TestConfig implements CommandLineRunner {
     User obj5 = new User(null, "carlos@hotmail.com", "192938", User.Role.ROLE_USUARIO);
 
     userRepository.saveAll(Arrays.asList(obj1, obj2, obj3, obj4, obj5));
+
+    Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
+		Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
+		Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
+		Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
+		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
+
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 
   }
 }
