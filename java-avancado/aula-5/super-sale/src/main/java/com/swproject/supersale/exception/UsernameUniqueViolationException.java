f85f99548263357b848e5974e2399a12d9adfd4c
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2024-03-21 17:27:40 -0300

    aula-5

---
+++ java-avancado/aula-5/super-sale/src/main/java/com/swproject/supersale/exception/UsernameUniqueViolationException.java
@@ -0,0 +1,8 @@
+package com.swproject.supersale.exception;
+
+public class UsernameUniqueViolationException extends RuntimeException {
+
+    public UsernameUniqueViolationException(String message) {
+        super(message);
+    }
+}
---
+++ java-avancado/aula-5/super-sale/src/main/java/com/swproject/supersale/repositories/CategoryRepository.java
@@ -0,0 +1,11 @@
+package com.swproject.supersale.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.swproject.supersale.entities.Category;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long>{
+  
+}
