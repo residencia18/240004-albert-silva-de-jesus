17e43095a108a7c73e134c51383f5fbca2f05770
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2024-03-22 19:50:09 -0300

    TestConfig e pom.xml

--- java-avancado/aula-8/shopall/src/main/java/com/swproject/shopall/config/TestConfig.java
+++ java-avancado/aula-8/shopall/src/main/java/com/swproject/shopall/config/TestConfig.java
@@ -1,10 +1,7 @@
 package com.swproject.shopall.config;
 
-import java.text.SimpleDateFormat;
 import java.time.Instant;
-import java.util.Arrays;
 import java.util.Locale;
-import java.util.TimeZone;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,6 +21,8 @@ import com.swproject.shopall.repositories.EmployeeRepository;
 import com.swproject.shopall.repositories.ProductRepository;
 import com.swproject.shopall.repositories.UsuarioRepository;
 
+import br.com.caelum.stella.tinytype.CPF;
+
 @Configuration
 @Profile("test")
 public class TestConfig implements CommandLineRunner {
@@ -42,115 +41,60 @@ public class TestConfig implements CommandLineRunner {
   @Autowired
   private EmployeeRepository employeeRepository;
 
-  @SuppressWarnings("null")
   @Override
   public void run(String... args) throws Exception {
 
-    SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-    sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
-
-    Faker faker = new Faker(new Locale("en"));
-
-    // Category cat1 = new Category(null, "Electronics");
-    // Category cat2 = new Category(null, "Books");
-    // Category cat3 = new Category(null, "Computers");
-
-    // Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
-    // Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
-    // Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
-    // Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
-    // Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
-
-    // categoryRepositoy.saveAll(Arrays.asList(cat1, cat2, cat3));
-    // productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
-
-    // p1.getCategories().add(cat2);
-    // p2.getCategories().add(cat1);
-    // p3.getCategories().add(cat3);
-    // p4.getCategories().add(cat3);
-    // p5.getCategories().add(cat2);
-
-    // productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+    Faker faker = new Faker(new Locale("pt-BR"));
 
     for (int i = 0; i < 5; i++) {
       // Gerando uma data de nascimento aleatória
       Instant randomBirthDate = faker.date().birthday().toInstant();
 
+      // Criando uma nova categoria com os dados aleatórios
+      Category category = new Category();
+      category.setName(faker.commerce().department());
+      categoryRepositoy.save(category);
+      log.info("Category: {}", category);
+
+      // Criando um novo produto com os dados aleatórios
+      Product product = new Product();
+      product.setName(faker.commerce().productName());
+      product.setDescription(faker.commerce().material());
+      product.setPrice(faker.number().randomDouble(2, 10, 1000));
+      productRepository.save(product);
+      log.info("Product: {}", product);
+
+      // Atribuindo a categoria ao produto
+      product.getCategories().add(category);
+      productRepository.save(product);
+      log.info("Product: {}", product);
+
+      // Gerar um número de CPF aleatório
+      String cpfString = faker.number().digits(11);
+
+      // Obter o CPF formatado
+      String cpfFormatado = new CPF(cpfString).getNumeroFormatado();
+
       // Criando um novo Employee com os dados aleatórios
       Employee employee = new Employee();
       employee.setName(faker.name().fullName());
-      employee.setCpf(faker.number().digits(11));
+      employee.setCpf(cpfFormatado);
       employee.setBirthDate(randomBirthDate);
 
-      // Criando um novo Usuario com os dados aleatórios
-      Usuario usuario = createUsuario(i + 1, faker);
-      employee.setUsuario(usuario);
+      // Criando um novo usuário com os dados aleatórios
+      Usuario usuario = new Usuario();
+      usuario.setUsername(faker.internet().emailAddress());
+      usuario.setPassword(faker.number().digits(6));
+      usuario.setPerfilTipo(PerfilTipo.FUNCIONARIO);
+      userRepository.save(usuario);
+      log.info("Usuario: {}", usuario);
 
-      // Criando um novo Produto com os dados aleatórios
-      Product product = createProduct(i + 1);
-      // Atribuindo o produto ao employee
+      // Atribuindo o usuário ao funcionário
       employee.getProductsSold().add(product);
-
-      Category category = createCategory(i + 1);
-      product.getCategories().add(category);
-      productRepository.save(product);
-
+      employee.setUsuario(usuario);
       employeeRepository.save(employee);
       log.info("Employee: {}", employee);
     }
   }
 
-  private Category createCategory(int index) {
-    switch (index) {
-      case 1:
-        return categoryRepositoy.save(new Category(null, "Electronics"));
-      case 2:
-        return categoryRepositoy.save(new Category(null, "Books"));
-      case 3:
-        return categoryRepositoy.save(new Category(null, "Computers"));
-      default:
-        return null;
-    }
-  }
-
-  private Product createProduct(int index) {
-    switch (index) {
-      case 1:
-        return productRepository
-            .save(new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, ""));
-      case 2:
-        return productRepository
-            .save(new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, ""));
-      case 3:
-        productRepository.save(new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, ""));
-      case 4:
-        return productRepository
-            .save(new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, ""));
-      case 5:
-        productRepository
-            .save(new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, ""));
-      default:
-        return null;
-    }
-  }
-
-  private Usuario createUsuario(int index, Faker faker) {
-    switch (index) {
-      case 1:
-        return userRepository.save(new Usuario(null, faker.internet().emailAddress(), "123456", PerfilTipo.ADMIN));
-      case 2:
-        return userRepository
-            .save(new Usuario(null, faker.internet().emailAddress(), "098765", PerfilTipo.FUNCIONARIO));
-      case 3:
-        return userRepository
-            .save(new Usuario(null, faker.internet().emailAddress(), "234567", PerfilTipo.FUNCIONARIO));
-      case 4:
-        return userRepository
-            .save(new Usuario(null, faker.internet().emailAddress(), "987654", PerfilTipo.FUNCIONARIO));
-      case 5:
-        return userRepository.save(new Usuario(null, faker.internet().emailAddress(), "192938", PerfilTipo.ADMIN));
-      default:
-        return null;
-    }
-  }
 }
