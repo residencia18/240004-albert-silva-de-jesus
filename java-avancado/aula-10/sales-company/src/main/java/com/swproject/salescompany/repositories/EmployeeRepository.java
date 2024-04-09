d46251c4cc9bbf564d7ab29f8d2f636d25a1c501
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2024-04-08 22:21:44 -0300

    EmployeeRepositoryTest, EmployeeRepository e ProductRepository

--- java-avancado/aula-10/sales-company/src/main/java/com/swproject/salescompany/repositories/EmployeeRepository.java
+++ java-avancado/aula-10/sales-company/src/main/java/com/swproject/salescompany/repositories/EmployeeRepository.java
@@ -1,5 +1,7 @@
 package com.swproject.salescompany.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,5 @@ import com.swproject.salescompany.entities.Employee;
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long>{
   
+  Optional<Employee> findByName(String name);
 }
