39bdd04b1861bfadaa11ed82b56050480beca2a0
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2024-04-09 16:15:49 -0300

    createCategory_WithExistingNome_ThrowsException

--- java-avancado/aula-10/sales-company/src/test/java/com/swproject/salescompany/repository/CategoryRepositoryTest.java
+++ java-avancado/aula-10/sales-company/src/test/java/com/swproject/salescompany/repository/CategoryRepositoryTest.java
@@ -3,16 +3,17 @@ package com.swproject.salescompany.repository;
 import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.dao.DataIntegrityViolationException;
 
 import com.github.javafaker.Faker;
 import com.swproject.salescompany.entities.Category;
-import com.swproject.salescompany.entities.Employee;
 import com.swproject.salescompany.repositories.CategoryRepository;
 
 @DataJpaTest
@@ -63,5 +64,19 @@ public class CategoryRepositoryTest {
         assertThat(savedCategory.getId()).isGreaterThan(0); // Não é muito útil nesse caso
         assertThat(savedCategory.getName()).isEqualTo(category.getName());
 
+    }
+
+    @Test
+    void createCategory_WithExistingNome_ThrowsException() {
+        // Crie e persista uma categoria com um nome existente
+        Category category1 = generateFakeCategory();
+        testEntityManager.persistFlushFind(category1); // Força a persistência imediata
+
+        // Tente criar outra categoria com o mesmo nome
+        Category category2 = generateFakeCategory();
+        category2.setName(category1.getName());
+
+        // Verifique se uma exceção é lançada ao tentar salvar a segunda categoria
+        assertThatThrownBy(() -> categoryRepository.save(category2)).isInstanceOf(Exception.class);
     }
 }
