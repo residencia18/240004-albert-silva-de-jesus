bb4f2164e2033cf06aa816090c99b2a756d81be2
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2024-04-09 17:07:20 -0300

    aula-11

---
+++ java-avancado/aula-11/general-sales/src/test/java/com/swprojects/generalsales/GeneralSalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swprojects.generalsales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeneralSalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
