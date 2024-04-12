d84e9f28e11f3ee03b0055bf9208533baa3ec168
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2024-04-11 22:32:40 -0300

    aula-12 preparada para aula

---
+++ java-avancado/aula-12/product-sales/src/test/java/com/swprojets/productsales/ProductSalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swprojets.productsales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductSalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
