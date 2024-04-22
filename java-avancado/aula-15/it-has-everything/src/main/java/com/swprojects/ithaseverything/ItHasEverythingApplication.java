0fbb010dc2e7ea2d598fd9619595e9bf3b8d85bd
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2024-04-22 16:01:47 -0300

    Projeto spring preparado para a aula-15

---
+++ java-avancado/aula-15/it-has-everything/src/main/java/com/swprojects/ithaseverything/ItHasEverythingApplication.java
@@ -0,0 +1,13 @@
+package com.swprojects.ithaseverything;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItHasEverythingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItHasEverythingApplication.class, args);
+	}
+
+}
