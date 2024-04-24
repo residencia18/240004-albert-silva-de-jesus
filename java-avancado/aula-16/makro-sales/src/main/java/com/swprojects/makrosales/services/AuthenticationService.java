fba5c90093f0cd60f827b56e3d337b49f31d5a29
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2024-04-24 19:54:09 -0300

    AuthenticationService

--- java-avancado/aula-16/makro-sales/src/main/java/com/swprojects/makrosales/services/AuthenticationService.java
+++ java-avancado/aula-16/makro-sales/src/main/java/com/swprojects/makrosales/services/AuthenticationService.java
@@ -1,5 +1,42 @@
 package com.swprojects.makrosales.services;
 
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.swprojects.makrosales.entities.User;
+import com.swprojects.makrosales.repositories.UserRepository;
+import com.swprojects.makrosales.security.JwtProvider;
+import com.swprojects.makrosales.web.dto.LoginRequestDto;
+import com.swprojects.makrosales.web.dto.LoginResponseDto;
+import com.swprojects.makrosales.web.dto.RegisterRequestDto;
+import com.swprojects.makrosales.web.dto.mapper.UserMapper;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
 public class AuthenticationService {
-  
+
+  private final UserRepository userRepository;
+  private final PasswordEncoder passwordEncoder;
+  private final AuthenticationManager authenticationManager;
+  private final JwtProvider jwtProvider;
+  private final UserMapper userMapper;
+
+  public void register(RegisterRequestDto registerRequest) {
+    User user = userMapper.fromRegisterRequest(registerRequest);// Usando o UserMapper para criar o objeto User
+    user.setPassword(passwordEncoder.encode(user.getPassword()));
+    userRepository.saveUser(user);
+  }
+
+  public LoginResponseDto login(LoginRequestDto loginRequest) {
+    Authentication authenticate = authenticationManager
+        .authenticate(new UsernamePasswordAuthenticationToken(loginRequest.username(),
+            loginRequest.password()));
+    String token = jwtProvider.generateToken(authenticate);
+    return new LoginResponseDto(token);
+  }
 }
