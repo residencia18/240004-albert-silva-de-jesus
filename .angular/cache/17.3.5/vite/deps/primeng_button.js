ea715c7eae3e94c8f99d268bf94902a0027569a2
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2024-04-24 00:31:35 -0300

    FEA-P010

---
+++ .angular/cache/17.3.5/vite/deps/primeng_button.js
@@ -0,0 +1,14 @@
+import {
+  Button,
+  ButtonDirective,
+  ButtonModule
+} from "./chunk-MTOXE2W5.js";
+import "./chunk-2AI4BDQZ.js";
+import "./chunk-CVLI7GHA.js";
+import "./chunk-P6BN7I6E.js";
+export {
+  Button,
+  ButtonDirective,
+  ButtonModule
+};
+//# sourceMappingURL=primeng_button.js.map
