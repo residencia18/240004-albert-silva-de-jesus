d2534183c8f9a02f20d039366a244fe1f3a41b84
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2024-04-24 21:20:35 -0300

    Pom.xml, User, AuthController, DashboardController, app.key, app.pub e V012__create_table_tb_user.sql

--- java-avancado/aula-16/makro-sales/src/main/java/com/swprojects/makrosales/entities/User.java
+++ java-avancado/aula-16/makro-sales/src/main/java/com/swprojects/makrosales/entities/User.java
@@ -3,6 +3,7 @@ package com.swprojects.makrosales.entities;
 import com.swprojects.makrosales.validation.ValidPassword;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -18,6 +19,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Entity
 public class User {
 
     @Id
--- java-avancado/aula-16/makro-sales/src/main/java/com/swprojects/makrosales/web/controllers/AuthController.java
+++ java-avancado/aula-16/makro-sales/src/main/java/com/swprojects/makrosales/web/controllers/AuthController.java
@@ -1,5 +1,33 @@
 package com.swprojects.makrosales.web.controllers;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.swprojects.makrosales.services.AuthenticationService;
+import com.swprojects.makrosales.web.dto.LoginRequestDto;
+import com.swprojects.makrosales.web.dto.LoginResponseDto;
+import com.swprojects.makrosales.web.dto.RegisterRequestDto;
+import com.swprojects.makrosales.web.dto.RegisterResponseDto;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
 public class AuthController {
-  
+
+  private final AuthenticationService authenticationService;
+
+  @PostMapping("/register")
+  public RegisterResponseDto register(@RequestBody RegisterRequestDto registerRequest) {
+    authenticationService.register(registerRequest);
+    return new RegisterResponseDto("User registered successfully");
+  }
+
+  @PostMapping("/login")
+  public LoginResponseDto login(@RequestBody LoginRequestDto loginRequest) {
+    return authenticationService.login(loginRequest);
+  }
 }
