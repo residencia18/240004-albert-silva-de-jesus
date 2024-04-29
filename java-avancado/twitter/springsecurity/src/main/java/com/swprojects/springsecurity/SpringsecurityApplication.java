4f5effc4ac532f8b7f0d1cc091763844764c1890
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2024-04-29 11:10:22 -0300

    Entities criadas para o projeto tweeter

---
+++ java-avancado/twitter/springsecurity/src/main/java/com/swprojects/springsecurity/SpringsecurityApplication.java
@@ -0,0 +1,13 @@
+package com.swprojects.springsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringsecurityApplication.class, args);
+	}
+
+}
---
+++ java-avancado/twitter/springsecurity/src/test/java/com/swprojects/springsecurity/SpringsecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swprojects.springsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
