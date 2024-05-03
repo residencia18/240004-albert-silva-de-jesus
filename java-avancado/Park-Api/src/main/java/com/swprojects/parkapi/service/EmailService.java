708519deb832d08198a99cd6927abd80f4b8abcf
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2024-05-03 12:06:47 -0300

    EmailService

---
+++ java-avancado/Park-Api/src/main/java/com/swprojects/parkapi/service/EmailService.java
@@ -0,0 +1,43 @@
+package com.swprojects.parkapi.service;
+
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+
+@Service
+public class EmailService {
+
+  @Autowired
+  private JavaMailSender javaMailSender;
+
+  @Autowired
+  private SpringTemplateEngine templateEngine;
+
+  public void enviarPedidoDeConfirmacaoDeCadastro(String destino, String codigo) throws MessagingException {
+    MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+    MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, MimeMessageHelper.MULTIPART_MODE_MIXED_RELATED,
+        "UTF-8");
+
+    Context context = new Context();
+    context.setVariable("titulo", "Bem-vindo ao ParkApi");
+    context.setVariable("texto", "Precisamos que você confirme seu cadastro clicando no link abaixo");
+    context.setVariable("linkConfirmacao", "http://localhost:8080/api/v1/usuarios?codigo=" + codigo);
+
+    String html = templateEngine.process("email/confirmacao", context);
+    helper.setTo(destino);
+    helper.setText(html, true);
+    helper.setSubject("Confirmação de cadastro");
+    helper.setFrom("nao-responder@park.com.br");
+
+    helper.addInline("logo", new ClassPathResource("/static/image/spring-security.png"));
+
+    javaMailSender.send(mimeMessage);
+  }
+}
