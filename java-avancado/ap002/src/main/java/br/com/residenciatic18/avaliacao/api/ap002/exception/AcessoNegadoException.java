6276dac55256cf6231715a8837f84948543a40a0
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2024-05-11 19:49:50 -0300

    Exceptions

---
+++ java-avancado/ap002/src/main/java/br/com/residenciatic18/avaliacao/api/ap002/exception/AcessoNegadoException.java
@@ -0,0 +1,8 @@
+package br.com.residenciatic18.avaliacao.api.ap002.exception;
+
+public class AcessoNegadoException extends RuntimeException {
+
+	public AcessoNegadoException(String message) {
+		super(message);
+	}
+}
