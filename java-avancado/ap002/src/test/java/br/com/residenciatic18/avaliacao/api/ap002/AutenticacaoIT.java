b14c5588f874617075a663f77b168ce7b2ad6356
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2024-05-11 20:36:04 -0300

    AutenticacaoIT

--- java-avancado/ap002/src/test/java/br/com/residenciatic18/avaliacao/api/ap002/AutenticacaoIT.java
+++ java-avancado/ap002/src/test/java/br/com/residenciatic18/avaliacao/api/ap002/AutenticacaoIT.java
@@ -62,4 +62,75 @@ public class AutenticacaoIT {
     org.assertj.core.api.Assertions.assertThat(responseBody.getStatus()).isEqualTo(400);
   }
 
+  @Test
+  public void autenticar_ComUsernameInvalido_RetornarErrorMessageStatus422() {
+    ErrorMessage responseBody = testClient
+        .post()
+        .uri("/api/v1/auth")
+        .contentType(MediaType.APPLICATION_JSON)
+        .bodyValue(new UsuarioLoginDto("", "JAVA!@#ResTIc18"))
+        .exchange()
+        .expectStatus().isEqualTo(400)
+        .expectBody(ErrorMessage.class)
+        .returnResult().getResponseBody();
+
+    org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
+    org.assertj.core.api.Assertions.assertThat(responseBody.getStatus()).isEqualTo(400);
+
+    responseBody = testClient
+        .post()
+        .uri("/api/v1/auth")
+        .contentType(MediaType.APPLICATION_JSON)
+        .bodyValue(new UsuarioLoginDto("@email.com", "JAVA!@#ResTIc18"))
+        .exchange()
+        .expectStatus().isEqualTo(422)
+        .expectBody(ErrorMessage.class)
+        .returnResult().getResponseBody();
+
+    org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
+    org.assertj.core.api.Assertions.assertThat(responseBody.getStatus()).isEqualTo(422);
+  }
+
+  @Test
+  public void autenticar_ComPasswordInvalido_RetornarErrorMessageStatus422() {
+    ErrorMessage responseBody = testClient
+        .post()
+        .uri("/api/v1/auth")
+        .contentType(MediaType.APPLICATION_JSON)
+        .bodyValue(new UsuarioLoginDto("ana@email.com", "JAVA!@#ResT"))
+        .exchange()
+        .expectStatus().isEqualTo(422)
+        .expectBody(ErrorMessage.class)
+        .returnResult().getResponseBody();
+
+    org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
+    org.assertj.core.api.Assertions.assertThat(responseBody.getStatus()).isEqualTo(422);
+
+    responseBody = testClient
+        .post()
+        .uri("/api/v1/auth")
+        .contentType(MediaType.APPLICATION_JSON)
+        .bodyValue(new UsuarioLoginDto("ana@email.com", "ResTIc1"))
+        .exchange()
+        .expectStatus().isEqualTo(422)
+        .expectBody(ErrorMessage.class)
+        .returnResult().getResponseBody();
+
+    org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
+    org.assertj.core.api.Assertions.assertThat(responseBody.getStatus()).isEqualTo(422);
+
+    responseBody = testClient
+        .post()
+        .uri("/api/v1/auth")
+        .contentType(MediaType.APPLICATION_JSON)
+        .bodyValue(new UsuarioLoginDto("ana@email.com", "JAVA!@#ResTIc18dhdyf5"))
+        .exchange()
+        .expectStatus().isEqualTo(422)
+        .expectBody(ErrorMessage.class)
+        .returnResult().getResponseBody();
+
+    org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
+    org.assertj.core.api.Assertions.assertThat(responseBody.getStatus()).isEqualTo(422);
+  }
+
 }
