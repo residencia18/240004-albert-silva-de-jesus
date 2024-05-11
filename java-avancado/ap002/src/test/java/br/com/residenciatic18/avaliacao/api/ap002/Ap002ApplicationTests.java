c5bfc25c9486634b2feabe2e13264231fa1f0b6c
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2024-05-11 18:47:04 -0300

    ap002 Inicializada

---
+++ java-avancado/ap002/src/test/java/br/com/residenciatic18/avaliacao/api/ap002/Ap002ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.residenciatic18.avaliacao.api.ap002;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ap002ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
