1f5339f110806278058587197081239cd70e21db
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2024-05-13 11:37:55 -0300

    Pasta nova AP002

--- AP002/src/main/java/br/com/residenciatic18/avaliacao/api/ap002/exception/PasswordInvalidException.java
+++
@@ -1,8 +0,0 @@
-package br.com.residenciatic18.avaliacao.api.ap002.exception;
-
-public class PasswordInvalidException extends RuntimeException {
-
-    public PasswordInvalidException(String message) {
-        super(message);
-    }
-}
--- AP002/src/test/java/br/com/residenciatic18/avaliacao/api/ap002/repository/TokenRepositoryTest.java
+++
@@ -1,8 +0,0 @@
-package br.com.residenciatic18.avaliacao.api.ap002.repository;
-
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-
-@DataJpaTest
-public class TokenRepositoryTest {
-  
-}
